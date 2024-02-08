5002c3726f3cae28437b371ad025060591db31c5
Rigor <rigorsyguatbsn@gmail.com>
2024-02-08 22:43:47 +0800

    change navbar fixed to sticky

--- src/components/(layout)/Navbar.tsx
+++ src/components/(layout)/Navbar.tsx
@@ -18,19 +18,19 @@ import { Button } from '../ui/button'
 export default function Navbar() {
 
   return (
-    <nav className="w-full h-[70px] px-[1.2rem] py-[0.8rem] flex justify-between fixed z-50 top-0 left-0 dark:bg-gray-900 bg-opacity-100 backdrop-blur-2xl">
+    <nav className="w-full h-[70px] px-[1.2rem] py-[0.8rem] flex justify-between sticky z-50 top-0 left-0 dark:bg-gray-900 bg-opacity-100 backdrop-blur-2xl">
       <div className="flex flex-row justify-between items-center w-full max-w-screen-2xl mx-auto">
 
         {/* Left-side  */}
         <div className='flex items-center'>
-          
+
           <div className=''>
             <Link href='/'>
               <h1 className='font-extrabold text-2xl'>Jarsnft</h1>
             </Link>
-            
+
           </div>
-          
+
           <div className='hidden lg:flex'>
             <Link href='/create' className='px-3 font-semibold hover:text-zinc-500 cursor-pointer'>Create</Link>
             <Link href="/collection" className='px-3 font-semibold hover:text-zinc-500 cursor-pointer'>Collections</Link>
@@ -46,7 +46,7 @@ export default function Navbar() {
           {/* <AuthLoginButton /> */}
           <ConnectWeb3 />
         </div>
-        
+
       </div>
     </nav>
   )
