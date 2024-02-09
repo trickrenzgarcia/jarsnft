b2a85486083be4e4331481c93ac604623203ba4e
Rigor <rigorsyguatbsn@gmail.com>
2024-02-09 10:21:13 +0800

    update index.tsx

--- src/components/(layout)/index.tsx
+++ src/components/(layout)/index.tsx
@@ -1,3 +1,4 @@
 export { default as Navbar } from "./Navbar"
 export { default as Footer } from "./Footer"
 export { default as PopularCollections } from "./PopularCollections"
+export { default as Hero } from "./Hero"
