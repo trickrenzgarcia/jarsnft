e8a531f024d6027bf3cf06800046f4efe752a6a4
Rigor <rigorsyguatbsn@gmail.com>
2024-02-09 10:23:54 +0800

    update Hero.tsx file

--- src/components/(layout)/Hero.tsx
+++ src/components/(layout)/Hero.tsx
@@ -6,6 +6,7 @@ export default function Hero() {
     return (
         <div className='flex flex-col'>
             <Image src="@/public/assets/hero-icon.svg" width={1440} height={500} sizes='100vw' style={{ width: '100%', height: 'auto' }} alt='' />
+            <h1>Ayaw gumana ng image sa hero.tsx</h1>
         </div>
     )
 }
