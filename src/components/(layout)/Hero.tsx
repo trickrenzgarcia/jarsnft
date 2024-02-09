be948b87f2867e2e9f5844a379b169fb7bddb719
Rigor <rigorsyguatbsn@gmail.com>
2024-02-09 10:21:04 +0800

    add Hero.tsx file

---
+++ src/components/(layout)/Hero.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import Image from 'next/image'
+
+export default function Hero() {
+    return (
+        <div className='flex flex-col'>
+            <Image src="@/public/assets/hero-icon.svg" width={1440} height={500} sizes='100vw' style={{ width: '100%', height: 'auto' }} alt='' />
+        </div>
+    )
+}
