37bab436c1f29cd4ce3efd8b1c81e9c7c7717e1e
Rigor <rigorsyguatbsn@gmail.com>
2024-02-09 22:01:36 +0800

    fix banner image not showing

--- src/components/(layout)/Hero.tsx
+++ src/components/(layout)/Hero.tsx
@@ -1,12 +1,12 @@
 "use client"
 
 import Image from 'next/image'
+import heroBanner from '../../../public/assets/hero-icon.svg'
 
 export default function Hero() {
     return (
         <div className='flex flex-col'>
-            <Image src="@/public/assets/hero-icon.svg" width={1440} height={500} sizes='100vw' style={{ width: '100%', height: 'auto' }} alt='' />
-            <h1>Ayaw gumana ng image sa hero.tsx</h1>
+            <Image src={heroBanner} width={1440} height={500} sizes='100vw' style={{ width: '100%', height: 'auto' }} alt='' />
         </div>
     )
 }
