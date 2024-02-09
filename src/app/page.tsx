adacfb93ba2664d2340c8ebf456b1c4914accccf
Rigor <rigorsyguatbsn@gmail.com>
2024-02-09 10:20:52 +0800

    add Hero component in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import { Navbar, PopularCollections, Footer } from "@/components/(layout)";
+import { Navbar, Hero, PopularCollections, Footer } from "@/components/(layout)";
 
 export default async function Home() {
 
   return (
     <main className="">
       <Navbar />
+      <Hero />
       <PopularCollections />
       <Footer />
     </main>
