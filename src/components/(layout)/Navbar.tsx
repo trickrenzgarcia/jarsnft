9f710c75b51f61df84e4078122f29c74b36f83ba
Rigor <rigorsyguatbsn@gmail.com>
2024-02-10 20:28:08 +0800

    add display prop to Navbar

--- src/components/(layout)/Navbar.tsx
+++ src/components/(layout)/Navbar.tsx
@@ -15,11 +15,16 @@ import { useTheme } from 'next-themes'
 import { AuthLoginButton, ConnectWeb3, LoginButton } from '../(interfaces)'
 import { Button } from '../ui/button'
 
-export default function Navbar() {
+type DisplayType = "fixed" | "sticky";
+interface NavbarProps {
+  display: DisplayType;
+}
+
+export default function Navbar({ display }: NavbarProps) {
 
   return (
-    <nav className="w-full h-[70px] px-[1.2rem] py-[0.8rem] flex justify-between fixed z-50 top-0 left-0 
-    bg-white bg-opacity-75 dark:bg-gray-900 dark:bg-opacity-75 backdrop-blur-sm border-b-2">
+    <nav className={`${display} w-full h-[70px] px-[1.2rem] py-[0.8rem] flex justify-between z-50 top-0 left-0 
+    bg-white bg-opacity-75 dark:bg-gray-900 dark:bg-opacity-75 backdrop-blur-sm border-b-2`}>
       <div className="flex flex-row justify-between items-center w-full max-w-screen-2xl mx-auto">
 
         {/* Left-side  */}
