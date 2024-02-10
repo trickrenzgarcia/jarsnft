66976610961555e0922979dea5b3ded97f168615
Rigor <rigorsyguatbsn@gmail.com>
2024-02-10 18:08:43 +0800

    add text in Hero section

--- src/components/(layout)/Hero.tsx
+++ src/components/(layout)/Hero.tsx
@@ -5,8 +5,14 @@ import heroBanner from '../../../public/assets/hero-icon.svg'
 
 export default function Hero() {
     return (
-        <div className='flex flex-col'>
-            <Image src={heroBanner} alt='' /> {/* gumana kahit walang width, height? */}
-        </div>
+        <section className='flex flex-col'>
+            <Image src={heroBanner} alt='image of a banner' /> {/* gumana kahit walang width, height? */}
+            <div className="absolute mt-48 ml-28">
+                <h1 className='text-[2rem] font-bold'>Unlock the Future: Collect, Create,</h1>
+                <h1 className='text-[2rem] font-bold'>and Conquer with NFTs!</h1>
+                <button className='mt-6 bg-white bg-opacity-90 dark:bg-gray-700 dark:bg-opacity-90 
+                px-10 py-3 rounded-xl'>Discover</button>
+            </div>
+        </section>
     )
 }
