c116a323a9b841906f100e73a2979e73586f6ebf
Patrick Garcia <trickrenzgarcia@gmail.com>
2024-02-27 02:57:48 +0800

    new screen size

--- src/components/(layout)/PopularCollections.tsx
+++ src/components/(layout)/PopularCollections.tsx
@@ -5,7 +5,7 @@ import { NFTContentSkeleton } from '../(skeletons)';
 export default async function PopularCollections() {
     return (
         <section className=""> {/* mt-10 */}
-            <div className="2.xl:w-[1456px] xl:w-[1280px] lg:w-[1024px] md:w-[768px] mx-auto">
+            <div className="2xl:w-[1456px] xl:w-[1456px] lg:w-[1280px] md:w-[768px] mx-auto">
                 <Suspense fallback={<NFTContentSkeleton />}>
                     <NFTContentWrapper title='Popular Collection' />
                 </Suspense>  
