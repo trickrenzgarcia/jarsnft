fa37f4a4c0a07299c723006b82dd9c340d1336d8
Jepz Dapar <106528202+HeyJepz@users.noreply.github.com>
2024-02-26 18:56:30 +0800

    Insight page content fixed

--- src/app/insights/page.tsx
+++ src/app/insights/page.tsx
@@ -1,11 +1,14 @@
+import { NFTContentWrapper } from "@/components/(interfaces)";
 import {Navbar} from "@/components/(layout)";
-import NewsCover from "@/components/(layout)/newsCover";
+import NewsCover from "@/components/(layout)/NewsCover";
 
-export default function insightPage(){
+export default function insightPage(){  
     return(
         <div>
+            {/* To Do: Add card for each article and a pop up screen when clicked */}
             <Navbar display='fixed' />
             <NewsCover />
+            <NFTContentWrapper title='Popular Collection' />
             <h1>Insights</h1>
         </div>
     );
