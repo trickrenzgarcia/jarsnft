44991179caf9a44ece92d95f7241c4ce1662083c
Patrick <trickrenzgarcia@gmail.com>
2024-03-03 22:00:16 +0800

    x

---
+++ environment.d.ts
@@ -0,0 +1,20 @@
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            NEXT_PUBLIC_APP_URL: string;
+            NEXT_PUBLIC_LOCAL_URL: string;
+            NEXT_PUBLIC_AUTH_SECRET: string;
+            NEXT_PUBLIC_THIRDWEB_AUTH_DOMAIN: string;
+            THIRDWEB_CLIENT_ID: string;
+            THIRDWEB_API_KEY: string;
+            THIRDWEB_AUTH_PRIVATE_KEY: string;
+            PRIVATE_KEY: string;
+            NEXT_PUBLIC_THIRDWEB_CLIENT_ID: string;
+            NEXT_PUBLIC_THIRDWEB_API_KEY: string;
+            NEXT_PUBLIC_BACKEND_URL: string;
+            JWT_AUTH_TOKEN: string;
+        }
+    }
+}
+
+export {}
