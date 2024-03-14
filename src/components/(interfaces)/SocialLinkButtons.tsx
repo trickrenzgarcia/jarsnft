f61b2508e89e152541652a6dcf66f68507e6ff45
Rigor <rigorsyguatbsn@gmail.com>
2024-03-14 23:22:34 +0800

    change modetoggle to dynamic

--- src/components/(interfaces)/SocialLinkButtons.tsx
+++ src/components/(interfaces)/SocialLinkButtons.tsx
@@ -2,31 +2,30 @@
 
 import { FaFacebookF, FaDiscord, FaTwitter, FaInstagram, FaGithub } from 'react-icons/fa'
 import { IconType } from 'react-icons';
-import { ModeToggle } from '.';
 import { useTheme } from 'next-themes';
 import Link from 'next/link';
+import dynamic from 'next/dynamic';
 
 export default function SocialLinkButtons() {
   const { theme, setTheme } = useTheme();
+  const ModeToggle = dynamic(() => import("../(interfaces)/ModeToggle"));
 
   return (
-    <>
-      <div className='px-8 lg:px-20 mb-24'>
-        <h1 className='text-2xl font-semibold mb-3'>Join us</h1>
-        <div className="flex justify-between">
-          <div className='flex gap-2'>
-            <SocialLinkButton Icon={FaFacebookF} link='' />
-            <SocialLinkButton Icon={FaDiscord} link='https://discord.gg/scBduTZyGd' />
-            <SocialLinkButton Icon={FaTwitter} link='' />
-            <SocialLinkButton Icon={FaInstagram} link='' />
-            <SocialLinkButton Icon={FaGithub} link='https://github.com/BroJavaDevs' />
-          </div>
-          <div>
-            <ModeToggle setTheme={setTheme} theme={theme} />
-          </div>
+    <div className='px-8 lg:px-20 mb-24'>
+      <h1 className='text-2xl font-semibold mb-3'>Join us</h1>
+      <div className="flex justify-between">
+        <div className='flex gap-2'>
+          <SocialLinkButton Icon={FaFacebookF} link='' />
+          <SocialLinkButton Icon={FaDiscord} link='https://discord.gg/scBduTZyGd' />
+          <SocialLinkButton Icon={FaTwitter} link='' />
+          <SocialLinkButton Icon={FaInstagram} link='' />
+          <SocialLinkButton Icon={FaGithub} link='https://github.com/BroJavaDevs' />
+        </div>
+        <div>
+          <ModeToggle setTheme={setTheme} theme={theme} />
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
