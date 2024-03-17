5c910dd1952260fcfd38f6a2fb767280cf5c3a33
Rigor <rigorsyguatbsn@gmail.com>
2024-03-17 16:11:27 +0800

    style tradehero section

--- src/app/trade/page.tsx
+++ src/app/trade/page.tsx
@@ -1,14 +1,19 @@
 "use client"
 
+import { Separator } from '@/components/ui/separator'
 import { ArtTrend, BuySellNFT, NFTCategories, PhotoTrend, TradeHero } from './_components'
 
 export default function TradePage() {
     return (
         <main className='container'>
             <TradeHero />
+            <Separator className='w-full h-[2px]' />
             <BuySellNFT />
+            <Separator className='w-full h-[2px]' />
             <ArtTrend />
+            <Separator className='w-full h-[2px]' />
             <PhotoTrend />
+            <Separator className='w-full h-[2px]' />
             <NFTCategories />
         </main>
     )
