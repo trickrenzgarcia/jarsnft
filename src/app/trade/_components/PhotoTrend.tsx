cb13b1abd50e7ee050757aad9379a9426e102587
Rigor <rigorsyguatbsn@gmail.com>
2024-03-18 10:35:33 +0800

    style arttrend & phototrend sections

--- src/app/trade/_components/PhotoTrend.tsx
+++ src/app/trade/_components/PhotoTrend.tsx
@@ -1,10 +1,18 @@
 "use client"
 
+import NFTCard from "./NFTCard"
+
 export default function PhotoTrend() {
     return (
         <>
             <div className="my-10">
-                <p className="font-bold text-xl">PhotoTrend</p>
+                <p className="font-bold text-xl mb-10">Trending in Photography</p>
+                <div className="grid grid-cols-4 gap-6">
+                    <NFTCard logo="/assets/logo-transparent.png" image="/assets/ex1.png" name="Bored Apes" verified={true} floor="43.2K" volume="363K" />
+                    <NFTCard logo="/assets/logo-transparent.png" image="/assets/ex1.png" name="Bored Apes" verified={true} floor="43.2K" volume="363K" />
+                    <NFTCard logo="/assets/jars256.png" image="/assets/ex2.png" name="Azuki" verified={false} floor="5.6K" volume="109.5K" />
+                    <NFTCard logo="/assets/jars256.png" image="/assets/ex2.png" name="Azuki" verified={false} floor="5.6K" volume="109.5K" />
+                </div>
             </div>
         </>
     )
