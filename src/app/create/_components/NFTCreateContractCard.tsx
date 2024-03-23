741da9519d37a44c485628e5d1a01e4cb535f835
Patrick <trickrenzgarcia@gmail.com>
2024-03-23 23:08:22 +0800

    xd

--- src/app/create/_components/NFTCreateContractCard.tsx
+++ src/app/create/_components/NFTCreateContractCard.tsx
@@ -33,26 +33,27 @@ import { Button } from "@/components/ui/button";
 import { ACCEPTED_IMAGE_TYPES } from "@/types/constant";
 import { FaExclamationCircle } from "react-icons/fa";
 import { Textarea } from "@/components/ui/textarea";
+import { useAddress } from "@thirdweb-dev/react";
 
 type NFTCreateContractCardProps = {
   title: string;
   description: string;
 };
 
+const addressOrENSSchema = z.string().regex(/^0x[a-fA-F0-9]{40}$/, "Input is not a valid address or ENS name.").min(1, "ENS name cannot be empty.")
+
 const ContractSchema = z.object({
-  name: z.string().min(1, {
-    message: "Required field.",
-  }).max(100, { message: "Name must be less than 100 characters."}),
+  name: z.string().min(1, "Required field.").max(100, "Name must be less than 100 characters."),
   image: z.instanceof(File, {
     message: "Required field.",
   }),
-  symbol: z.string().optional(),
+  symbol: z.string().min(1),
   description: z.string().optional(),
   // app_uri: z.string().min(1),
   // external_link: z.string().min(1),
-  fee_recipient: z.string().min(1),
-  // seller_fee_basis_points: z.number().min(0).max(100),
-  // primary_sale_recipient: z.string().min(1),
+  fee_recipient: addressOrENSSchema,
+  seller_fee_basis_points: z.string().regex(/^(100(\.0{1,3})?|0+(\.\d{1,3})?|([1-9]?\d(\.\d{1,3})?))$/, "Invalid percentage."),
+  primary_sale_recipient: addressOrENSSchema,
   // trusted_forwarders: z.array(z.string()).optional(),
 });
 
@@ -74,11 +75,9 @@ export default function NFTCreateContractCard({
       // description: "",
       // app_uri: "",
       // external_link: "",
-      // fee_recipient: "",
-      // seller_fee_basis_points: 0,
-      // primary_sale_recipient: "",
-      // trusted_forwarders: [],
-    },
+      fee_recipient: "",
+      seller_fee_basis_points: "0.00"
+    }
   });
 
   const onDrop = useCallback(
@@ -137,6 +136,54 @@ export default function NFTCreateContractCard({
       });
     }
   };
+
+  function handlePercentageChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const inputValue = e.target.value.trim(); // Trim whitespace
+
+    // Check if the input value is empty
+    if (inputValue === "") {
+        return; // Do nothing if the input is empty
+    }
+
+    
+
+    // Regular expression to match any character from A-Z or any special character
+    const regex = /^[A-Za-z!@#$%^&*()_+\-=\[\]{};':"\\|,<>\/?]*$/;
+
+    // Check if the input value is alphabetic or contains special characters
+    if (regex.test(inputValue)) {
+        e.target.value = "0.00";
+        return;
+    }
+
+    // Convert the input value to a number
+    const inputValueAsNumber = parseFloat(inputValue);
+
+    // Check if the input value is a number and greater than 100.0
+    if (!isNaN(inputValueAsNumber) && inputValueAsNumber > 100.0) {
+        e.target.value = "100.0";
+        return;
+    }
+  }
+
+  function handleNameChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const inputValue = e.target.value.trim(); // Trim whitespace
+
+    // Split the input value into an array of words
+    const words = inputValue.split(" ");
+    
+    // Map over the words array and extract the first letter of each word
+    const initials = words.map(word => word.charAt(0).toUpperCase());
+    
+    // Join the extracted initials together to form the acronym
+    const acronym = initials.join("");
+
+    if(acronym.length > 0) {
+      form.setValue("symbol", acronym);
+    } else {
+      form.setValue("symbol", "");
+    }
+  }
 
   function submitCreateContract(data: FormContract) {
     console.log(data, "Submitted!");
@@ -234,6 +281,7 @@ export default function NFTCreateContractCard({
                           {...field}
                           id="name"
                           placeholder="Enter the name of your NFT contract"
+                          onChange={handleNameChange}
                         />
                       </FormControl>
                     </FormItem>
@@ -251,7 +299,7 @@ export default function NFTCreateContractCard({
                         <Input
                           {...field}
                           id="symbol"
-                          placeholder="HAV"
+                          placeholder="HAV (Optional)"
                         />
                       </FormControl>
 
@@ -280,24 +328,54 @@ export default function NFTCreateContractCard({
               />
             </div>
 
-            <div className="pt-5">
-              <FormField
-                control={form.control}
-                name="fee_recipient"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="font-bold flex items-center gap-1">Royalties</FormLabel>
-                    <FormDescription className="text-sm">Determine the address that should receive the revenue from royalties earned from secondary sales of the assets.</FormDescription>
-                    <FormControl>
-                      <Input
-                        {...field}
-                        id="fee_recipient"
-                        placeholder=""
-                      />
-                    </FormControl>
-                  </FormItem>
-                )}
-              />
+            <div className="pt-5 grid grid-cols-12 gap-3">
+              <div className="col-span-9 md:col-span-10">
+                <FormField
+                  control={form.control}
+                  name="fee_recipient"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="font-bold flex items-center gap-1">Royalties</FormLabel>
+                      
+                      <FormControl>
+                        <Input
+                          {...field}
+                          id="fee_recipient"
+                          placeholder="address"
+                        />
+                      </FormControl>
+                      <FormDescription className="text-sm italic">Determine the address that should receive the revenue from royalties earned from secondary sales of the assets.</FormDescription>
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div className="col-span-3 md:col-span-2">
+                <FormField
+                  control={form.control}
+                  name="seller_fee_basis_points"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="font-bold flex items-center gap-1">Percentage</FormLabel>
+                      <FormControl>
+                        <Input
+                          {...form.register("seller_fee_basis_points")}
+                          id="seller_fee_basis_points"
+                          placeholder="0.00"
+                          maxLength={5}
+                          onChange={handlePercentageChange}
+                          onKeyDown={(e) => {
+                            if(e.key === ' ') {
+                              e.preventDefault();
+                              return;
+                            }
+                          }}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              </div>
+              
             </div>
             
             <Button type="submit">Create</Button>
