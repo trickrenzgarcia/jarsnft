d6aff381bdb2cc0eab2960508d81cfe4dad64ccf
Patrick <trickrenzgarcia@gmail.com>
2024-03-28 01:44:22 +0800

    added cool loading

--- src/app/create/deploy-contract/page.tsx
+++ src/app/create/deploy-contract/page.tsx
@@ -4,9 +4,20 @@ import NFTCreateContractCard from "../_components/NFTCreateContractCard";
 import DetailsCard from "../_components/DetailsCard";
 import ProfileCard from "../_components/ProfileCard";
 import { useUserContext } from "@/components/(providers)";
+import { motion } from "framer-motion";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function DeployContractPage() {
-  const { user, isLoading, isLoggedIn } = useUserContext();
+  const { user, isLoading } = useUserContext();
+
+  if(isLoading) {
+    return (
+      <div className="w-full flex flex-col h-[calc(100vh-57px)] items-center justify-center">
+        <LoadingLogo />
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
 
   return (
     <main className="w-full flex flex-col mx-auto md:flex-row justify-center md:py-8 gap-5">
@@ -21,5 +32,16 @@ export default function DeployContractPage() {
         <DetailsCard title="Once your contract deployment is complete, you'll have the ability to:" />
       </section>
     </main>
+  );
+}
+
+function LoadingLogo() {
+  return (
+    <motion.div
+      className="w-14 h-14 self-center"
+      // initial={{ opacity: 0.1, borderRadius: "10%", rotate: 180, backgroundColor: "#EB8253" }}
+      animate={{ opacity: 1, borderRadius: ['10%', '50%', '10%', '50%', '10%'], rotate: [180, 0, 180, 0, 180], backgroundColor: ['#EB8253', '#A3CCC1'] }}
+      transition={{ duration: 5, repeat: Infinity, ease: "easeOut" }}
+    ></motion.div>
   );
 }
