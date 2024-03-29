ecdbb7d5b14e5a2a07912bc76b933d7b5cc525fb
Rigor <rigorsyguatbsn@gmail.com>
2024-03-29 11:58:59 +0800

    change navbar to server-side

---
+++ src/components/(layout)/Login.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import {
+    useUser,
+} from "@thirdweb-dev/react";
+import { ConnectWeb3, CreateUserDialog, ProfileButton } from "../(interfaces)";
+import { ProfileQuery } from "@/types/users";
+
+export default function Login() {
+
+    const { user, isLoading, isLoggedIn } = useUser() as ProfileQuery;
+    return (
+        <div className="flex items-center">
+            {(isLoggedIn && (
+                <ProfileButton user={user} isUserLoading={isLoading} />
+            )) || <ConnectWeb3 btnTitle="Connect" />}
+
+            {isLoggedIn && !user.data.session.is_listed && (
+                (<CreateUserDialog isOpenCreate={!user.data.session.is_listed} />)
+            )}
+        </div>
+    )
+}
