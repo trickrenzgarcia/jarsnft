59c3118586afe9252413a66201daad52e8a28db8
AKCcoding <alraeatwork@gmail.com>
2024-04-04 21:56:15 +0800

    Update learn to Insights

---
+++ src/app/insights/faq/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { PageNextButton } from "../_components";
+
+export default function FAQsPage() {
+  return (
+    <>
+      <div>
+        FAQ
+        <PageNextButton title="Back to JarsNFT" href="/" />
+      </div>
+    </>
+  );
+}
--- src/app/insights/layout.tsx
+++ src/app/insights/layout.tsx
@@ -1,5 +1,10 @@
-import { SideNavLeft, SideNavRight, LearnNavbar, PageNavbarTitle } from './_components';
-import {Footer} from "@/components/(layout)";
+import {
+  SideNavLeft,
+  SideNavRight,
+  LearnNavbar,
+  PageNavbarTitle,
+} from "./_components";
+import { Footer } from "@/components/(layout)";
 
 type LearnProps = {
   children: React.ReactNode;
@@ -12,14 +17,14 @@ export default function LearnLayout({ children }: LearnProps) {
       <div className="container flex-1 items-start border-t-2 md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10">
         <SideNavLeft />
         <main className="relative py-6 lg:gap-10 lg:py-8 xl:grid xl:grid-cols-[1fr_300px]">
-          <div className="mx-auto h-[2000px] w-full min-w-0">
+          <div className="mx-auto h-screen w-full min-w-0">
             <PageNavbarTitle />
             {children}
           </div>
           <SideNavRight />
         </main>
       </div>
-      <Footer/>
+      <Footer />
     </main>
   );
 }
