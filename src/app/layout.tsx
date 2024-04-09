f173eec0ef1b1dbfcabf5fdf747905b5d27fada0
AKCcoding <alraeatwork@gmail.com>
2024-04-09 11:18:01 +0800

    Save bago sakuna

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={cn(
           "w-screen overflow-x-hidden antialiased",
