df5d9c4f58fd374e1e0a2fc198801e5a96b46fac
Rigor <rigorsyguatbsn@gmail.com>
2024-04-14 22:36:28 +0800

    add approve purchase dialog

--- src/app/uitest/page.tsx
+++ src/app/uitest/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
     <div className="flex flex-col justify-center items-center min-h-[100vh]">
       <BuyButton nftImageLink="/assets/ex1.png" nftName="MutantApeYacht #27046" nftCollectionName="Mutant Ape Yacht Club"
         isVerified={true} lastSalePrice={1.5} nftPrice={5.188} nftQuantity={1} nftPriceInDollars={11784.28}
-        userBalance={2.4} />
+        userBalance={2.4} networkChain="Polygon" />
       <SocialLinkButtons />
     </div>
   )
