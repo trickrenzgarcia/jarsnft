4b3bdcd6c7155a6b09a83de75577dc1ccf49e42f
Jepz Dapar <106528202+HeyJepz@users.noreply.github.com>
2024-04-15 15:29:05 +0800

    sticky sidenav

--- src/app/insights/page.tsx
+++ src/app/insights/page.tsx
@@ -31,6 +31,58 @@ export default function Page() {
         <li>Accepting an offer on your NFT</li>
         <li>Listing an NFT for sale a fixed price</li>
       </ul>
+
+      <h1 className="my-6 text-4xl font-bold">Welcome</h1>
+      <p className="my-12 tracking-wide">
+        Welcome to the JarsNft guides and frequently asked questions.
+        <br />
+        <br />
+        Contents:
+      </p>
+      <h2 className="my-6 text-2xl font-bold" id="getting-started">
+        Getting Started
+      </h2>
+      <ul className="mb-12 ml-8 font-semibold leading-9 text-[#A519D7] lg:list-disc">
+        <li>Installing a Wallet</li>
+        <li>Connecting your wallet</li>
+        <li>Creating your profile</li>
+      </ul>
+      <h2 className="my-6 text-2xl font-bold">Buying NFTs</h2>
+      <ul className="mb-12 ml-8 font-semibold leading-9 text-[#A519D7] lg:list-disc">
+        <li>Buying a fixed price NFT</li>
+        <li>Making an offer on a NFT</li>
+      </ul>
+      <h2 className="my-6 text-2xl font-bold">Selling NFTs</h2>
+      <ul className="mb-12 ml-8 font-semibold leading-9 text-[#A519D7] lg:list-disc">
+        <li>Accepting an offer on your NFT</li>
+        <li>Listing an NFT for sale a fixed price</li>
+      </ul>
+
+      <h1 className="my-6 text-4xl font-bold">Welcome</h1>
+      <p className="my-12 tracking-wide">
+        Welcome to the JarsNft guides and frequently asked questions.
+        <br />
+        <br />
+        Contents:
+      </p>
+      <h2 className="my-6 text-2xl font-bold" id="getting-started">
+        Getting Started
+      </h2>
+      <ul className="mb-12 ml-8 font-semibold leading-9 text-[#A519D7] lg:list-disc">
+        <li>Installing a Wallet</li>
+        <li>Connecting your wallet</li>
+        <li>Creating your profile</li>
+      </ul>
+      <h2 className="my-6 text-2xl font-bold">Buying NFTs</h2>
+      <ul className="mb-12 ml-8 font-semibold leading-9 text-[#A519D7] lg:list-disc">
+        <li>Buying a fixed price NFT</li>
+        <li>Making an offer on a NFT</li>
+      </ul>
+      <h2 className="my-6 text-2xl font-bold">Selling NFTs</h2>
+      <ul className="mb-12 ml-8 font-semibold leading-9 text-[#A519D7] lg:list-disc">
+        <li>Accepting an offer on your NFT</li>
+        <li>Listing an NFT for sale a fixed price</li>
+      </ul>
     </div>
   );
 }
