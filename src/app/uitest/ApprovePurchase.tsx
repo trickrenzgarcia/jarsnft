260ad1a218988d7278dffc1d32e362ea9b11f8ac
Rigor <rigorsyguatbsn@gmail.com>
2024-04-20 22:14:52 +0800

    change prop type identifier

--- src/app/uitest/ApprovePurchase.tsx
+++ src/app/uitest/ApprovePurchase.tsx
@@ -1,6 +1,6 @@
-"use client"
+"use client";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
@@ -9,11 +9,11 @@ import {
   DialogTitle,
   DialogTrigger,
   DialogFooter,
-} from "@/components/ui/dialog"
-import { Separator } from "@/components/ui/separator"
-import Image from "next/image"
+} from "@/components/ui/dialog";
+import { Separator } from "@/components/ui/separator";
+import Image from "next/image";
 
-type BuyButtonType = {
+type ApprovePurchaseType = {
   nftImageLink: string;
   nftName: string;
   nftCollectionName: string;
@@ -22,42 +22,67 @@ type BuyButtonType = {
   nftPrice: number;
   nftQuantity: number;
   nftPriceInDollars: number;
-}
+};
 
 export default function ApprovePurchase({
-  nftImageLink, nftName, nftCollectionName, isVerified, networkChain,
-  nftPrice, nftQuantity, nftPriceInDollars,
-}: BuyButtonType) {
+  nftImageLink,
+  nftName,
+  nftCollectionName,
+  isVerified,
+  networkChain,
+  nftPrice,
+  nftQuantity,
+  nftPriceInDollars,
+}: ApprovePurchaseType) {
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button variant="outline" className="bg-foreground-900 hover:bg-foreground-700 text-background
-         hover:text-background">CONFIRM AND PAY</Button>
+        <Button
+          variant="outline"
+          className="bg-foreground-900 text-background hover:bg-foreground-700
+         hover:text-background"
+        >
+          CONFIRM AND PAY
+        </Button>
       </DialogTrigger>
 
       <DialogContent className="min-w-[550px] p-0">
         {/* Header */}
         <DialogHeader className="px-8 pt-8">
-          <DialogTitle className="text-2xl font-bold">Approve Purchase</DialogTitle>
+          <DialogTitle className="text-2xl font-bold">
+            Approve Purchase
+          </DialogTitle>
         </DialogHeader>
 
         {/* Body */}
         <div className="flex justify-between px-8 py-4">
-          <Image src={nftImageLink} width={150} height={150} alt="image of an NFT" />
+          <Image
+            src={nftImageLink}
+            width={150}
+            height={150}
+            alt="image of an NFT"
+          />
 
           <div className="flex flex-col py-4">
-            <h1 className="text-2xl font-bold mb-2">{nftName}</h1>
+            <h1 className="mb-2 text-2xl font-bold">{nftName}</h1>
             <div className="flex gap-2">
               <h2>{nftCollectionName}</h2>
-              {isVerified && <Image src="/assets/verify.png" width={20} height={20} alt="verified logo" />}
+              {isVerified && (
+                <Image
+                  src="/assets/verify.png"
+                  width={20}
+                  height={20}
+                  alt="verified logo"
+                />
+              )}
             </div>
             <p className="mt-auto">Chain: {networkChain}</p>
           </div>
         </div>
 
         {/* Footer */}
-        <DialogFooter className="flex sm:flex-col sm:space-x-0 bg-neutral-200 dark:bg-neutral-800 rounded-t-3xl px-6 py-8">
-          <div className="flex justify-between text-xl font-bold px-4">
+        <DialogFooter className="flex rounded-t-3xl bg-neutral-200 px-6 py-8 dark:bg-neutral-800 sm:flex-col sm:space-x-0">
+          <div className="flex justify-between px-4 text-xl font-bold">
             <p>You pay</p>
             <p>{nftPrice} MATIC</p>
           </div>
@@ -67,9 +92,11 @@ export default function ApprovePurchase({
           </div>
           <Separator className="my-8" />
           <p className="px-4 text-neutral-500">Go to your wallet.</p>
-          <p className="px-4 text-neutral-500 mb-4">You&apos;ll be asked to approve this purchase from your wallet.</p>
+          <p className="mb-4 px-4 text-neutral-500">
+            You&apos;ll be asked to approve this purchase from your wallet.
+          </p>
         </DialogFooter>
       </DialogContent>
     </Dialog>
-  )
+  );
 }
--- src/app/uitest/PurchaseComplete.tsx
+++ src/app/uitest/PurchaseComplete.tsx
@@ -1,6 +1,6 @@
-"use client"
+"use client";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
@@ -9,48 +9,71 @@ import {
   DialogTitle,
   DialogTrigger,
   DialogFooter,
-} from "@/components/ui/dialog"
-import Image from "next/image"
-import Link from "next/link"
-import { IoOpenOutline } from "react-icons/io5"
+} from "@/components/ui/dialog";
+import Image from "next/image";
+import Link from "next/link";
+import { IoOpenOutline } from "react-icons/io5";
 
-type BuyButtonType = {
+type PurchaseCompleteType = {
   nftImageLink: string;
   nftName: string;
   nftCollectionName: string;
   transactionID: string;
   viewItemLink: string;
-}
+};
 
 export default function PurchaseComplete({
-  nftImageLink, nftName, nftCollectionName, transactionID, viewItemLink
-}: BuyButtonType) {
+  nftImageLink,
+  nftName,
+  nftCollectionName,
+  transactionID,
+  viewItemLink,
+}: PurchaseCompleteType) {
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button variant="outline" className="bg-foreground-900 hover:bg-foreground-700 text-background 
-        hover:text-background min-w-[200px]">Test Purchase Complete</Button>
+        <Button
+          variant="outline"
+          className="min-w-[200px] bg-foreground-900 text-background 
+        hover:bg-foreground-700 hover:text-background"
+        >
+          Test Purchase Complete
+        </Button>
       </DialogTrigger>
 
       <DialogContent className="min-w-[550px] p-0">
         {/* Body */}
         <div className="flex flex-col items-center justify-center py-10">
-          <Image className="mb-8" src={nftImageLink} width={150} height={150} alt="image of an NFT" />
-          <div className="text-center px-24">
+          <Image
+            className="mb-8"
+            src={nftImageLink}
+            width={150}
+            height={150}
+            alt="image of an NFT"
+          />
+          <div className="px-24 text-center">
             <h1 className="text-2xl font-bold">Your purchase is complete!</h1>
-            <p className="text-neutral-500">You purchased <span className="text-purple-500">{nftName}</span> from the <span className="text-purple-500">{nftCollectionName}</span> collection.</p>
+            <p className="text-neutral-500">
+              You purchased <span className="text-purple-500">{nftName}</span>{" "}
+              from the{" "}
+              <span className="text-purple-500">{nftCollectionName}</span>{" "}
+              collection.
+            </p>
           </div>
         </div>
 
         {/* Footer */}
-        <DialogFooter className="flex sm:flex-col sm:space-x-0 bg-neutral-200 dark:bg-neutral-800 rounded-t-3xl px-6 py-8">
-          <div className="text-center mb-8">
+        <DialogFooter className="flex rounded-t-3xl bg-neutral-200 px-6 py-8 dark:bg-neutral-800 sm:flex-col sm:space-x-0">
+          <div className="mb-8 text-center">
             <p>Transaction ID</p>
             <p className="text-purple-500">{transactionID}</p>
           </div>
           <Link href={viewItemLink}>
-            <Button variant="outline" className="flex align-middle bg-foreground-900 hover:bg-foreground-700 text-background
-                     hover:text-background font-bold w-full gap-1">
+            <Button
+              variant="outline"
+              className="flex w-full gap-1 bg-foreground-900 align-middle
+                     font-bold text-background hover:bg-foreground-700 hover:text-background"
+            >
               <p>View Item </p>
               <IoOpenOutline size={20} />
             </Button>
@@ -58,5 +81,5 @@ export default function PurchaseComplete({
         </DialogFooter>
       </DialogContent>
     </Dialog>
-  )
+  );
 }
