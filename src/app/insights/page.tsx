ee0d06a478dacf3405857ec8b853a9c00bccb721
AKCcoding <alraeatwork@gmail.com>
2024-04-23 16:49:45 +0800

    Protect Account Structure and content Done

--- src/app/insights/page.tsx
+++ src/app/insights/page.tsx
@@ -7,20 +7,25 @@ export default function Page() {
   return (
     <div>
       <h1 className="my-6 text-4xl font-bold">Welcome</h1>
-      <p className="my-12 text-base tracking-wide leading-loose">
-        Welcome to the JarsNft insights, the following are the overview of contents that will help you be informed in the basics of the Crypto world! 
+      <h2 className="font-serif text-3xl">
+        Please do your own research (DYOR) before investing
+      </h2>
+      <p className="my-12 text-base leading-loose tracking-wide">
+        Welcome to the JarsNft insights, the following are the overview of
+        contents that will help you be informed in the basics of the Crypto
+        world!
         <br />
         <br />
-        This page serves to help individuals especially beginners in getting a grasp of this emerging technology.
+        This page serves to help individuals especially beginners in getting a
+        grasp of this emerging technology.
       </p>
       <hr />
 
       <div id="getting-started">
-        <h3 className="my-6 text-2xl font-bold" >
-          Getting Started
-        </h3>
+        <h3 className="my-6 text-2xl font-bold">Getting Started</h3>
         <P>
-          This section will be your guide on where to begin your first transaction. Consider going through the following steps:
+          This section will be your guide on where to begin your first
+          transaction. Consider going through the following steps:
         </P>
         <h3 className="my-6 text-xl font-bold">1. Setup Your Wallet</h3>
 
@@ -28,19 +33,19 @@ export default function Page() {
           <li>Choose your Wallet</li>
           <li>Creating your Wallet Account </li>
         </ul>
-        
+
         <h3 className="my-6 text-xl font-bold">2. Create Your Profile</h3>
         <ul className="mb-12 ml-8 font-semibold leading-9 text-[#A519D7] lg:list-disc">
           <li>Customize your Profile</li>
         </ul>
-        
+
         <h3 className="my-6 text-xl font-bold">3. Create Your NFT</h3>
         <ul className="mb-12 ml-8 font-semibold leading-9 text-[#A519D7] lg:list-disc">
           <li>Creating your First NFT</li>
           <li>Setup your NFT details</li>
           <li>Minting your NFT</li>
         </ul>
-        
+
         <h3 className="my-6 text-xl font-bold">4. Trade Your NFT</h3>
         <ul className="mb-12 ml-8 font-semibold leading-9 text-[#A519D7] lg:list-disc">
           <li>Accepting an offer on your NFT</li>
@@ -50,11 +55,10 @@ export default function Page() {
       <hr />
 
       <div id="nfts">
-        <h2 className="my-6 text-2xl font-bold" >
-          Non-Fungible Token (NFT)
-        </h2>
+        <h2 className="my-6 text-2xl font-bold">Non-Fungible Token (NFT)</h2>
         <P>
-          This section will be your guide on where to begin your first transaction. Consider going through the following steps:
+          This section will be your guide on where to begin your first
+          transaction. Consider going through the following steps:
         </P>
         <h3 className="my-6 text-xl font-bold">1. What is an NFT?</h3>
 
@@ -62,16 +66,21 @@ export default function Page() {
           <li>Choose your Wallet</li>
           <li>Creating your Wallet Account </li>
         </ul>
-        <h3 className="my-6 text-xl font-bold">2. What makes an NFT Valuable?</h3>
+        <h3 className="my-6 text-xl font-bold">
+          2. What makes an NFT Valuable?
+        </h3>
         <ul className="mb-12 ml-8 font-semibold leading-9 text-[#A519D7] lg:list-disc">
           <li>Customize your Profile</li>
         </ul>
       </div>
       <hr />
 
-      <h2 className="my-6 text-2xl font-bold" id="blockchain">Blockchain</h2>
+      <h2 className="my-6 text-2xl font-bold" id="blockchain">
+        Blockchain
+      </h2>
       <P>
-        This section will be your guide on where to begin your first transaction. Consider going through the following steps:
+        This section will be your guide on where to begin your first
+        transaction. Consider going through the following steps:
       </P>
       <h3 className="my-6 text-xl font-bold">1. What Is The Blockchain?</h3>
 
@@ -83,14 +92,14 @@ export default function Page() {
       <ul className="mb-12 ml-8 font-semibold leading-9 text-[#A519D7] lg:list-disc">
         <li>Customize your Profile</li>
       </ul>
-      
+
       <h3 className="my-6 text-xl font-bold">3. What Are Gas Fees?</h3>
       <ul className="mb-12 ml-8 font-semibold leading-9 text-[#A519D7] lg:list-disc">
         <li>Creating your First NFT</li>
         <li>Setup your NFT details</li>
         <li>Minting your NFT</li>
       </ul>
-      
+
       <h3 className="my-6 text-xl font-bold">4. What Is Cryptocurrency?</h3>
       <ul className="mb-12 ml-8 font-semibold leading-9 text-[#A519D7] lg:list-disc">
         <li>Accepting an offer on your NFT</li>
@@ -101,10 +110,9 @@ export default function Page() {
   );
 }
 
-
 function P(props: React.HTMLAttributes<HTMLParagraphElement>) {
-    const { children } = props;
-    return <p className="my-8 tracking-wide text-sm leading-loose ">{children}</p>
-  }
-
-  
+  const { children } = props;
+  return (
+    <p className="my-8 text-sm leading-loose tracking-wide ">{children}</p>
+  );
+}
