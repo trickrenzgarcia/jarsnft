22bb018fced9018c086c171d20fcadd352f29e3a
Rigor <rigorsyguatbsn@gmail.com>
2024-05-09 23:12:12 +0800

    change styles

--- src/components/(layout)/TopNFTCollections.tsx
+++ src/components/(layout)/TopNFTCollections.tsx
@@ -3,10 +3,8 @@ import { TopNFTCollectionsTable } from "../(interfaces)";
 
 export default async function TopNFTCollections() {
   return (
-    <section className="my-16 px-0 sm:px-5 md:px-16">
-      <h1 className="mb-3 text-2xl font-bold text-left">
-        Top NFT Collections
-      </h1>
+    <section>
+      <h1 className="mb-3 text-left text-2xl font-bold">Top NFT Collections</h1>
       <Suspense fallback={<div>Loading...</div>}>
         <TopNFTCollectionsTable />
       </Suspense>
