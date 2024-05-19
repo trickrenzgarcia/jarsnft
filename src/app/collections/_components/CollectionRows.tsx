390c35c2fe7d812615991e34878b1129c87bc4dc
Rigor <rigorsyguatbsn@gmail.com>
2024-05-19 19:47:06 +0800

    add fake data

--- src/app/collections/_components/CollectionRows.tsx
+++ src/app/collections/_components/CollectionRows.tsx
@@ -4,57 +4,102 @@ import { Separator } from "@/components/ui/separator";
 import Link from "next/link";
 
 type CollectionRowsType = {
-    collectionHref: string;
-    collectionLogoSrc: string;
-    collectionName: string;
-    isVerified: boolean;
-    floorPrice: number;
-    floorChange: number;
-    volume: number;
-    volumeChange: number;
-    sales: number;
-    salesChange: number;
-    allCurrentListedNFTs: number;
-    allCurrentNFTS: number; // all NFTs currently in the collection, listed or not
-}
+  collectionHref: string;
+  collectionLogoSrc: string;
+  collectionName: string;
+  isVerified: boolean;
+  floorPrice: number;
+  floorChange: number;
+  volume: number;
+  volumeChange: number;
+  sales: number;
+  salesChange: number;
+  allCurrentListedNFTs: number;
+  allCurrentNFTS: number; // all NFTs currently in the collection, listed or not
+};
 
 export default function CollectionRows({
-    collectionHref, collectionLogoSrc, collectionName,
-    isVerified, floorPrice, floorChange, volume,
-    volumeChange, sales, salesChange, allCurrentListedNFTs, allCurrentNFTS
+  collectionHref,
+  collectionLogoSrc,
+  collectionName,
+  isVerified,
+  floorPrice,
+  floorChange,
+  volume,
+  volumeChange,
+  sales,
+  salesChange,
+  allCurrentListedNFTs,
+  allCurrentNFTS,
 }: CollectionRowsType) {
-    const isFloorChangeNegative = floorChange < 0;
-    const isVolumeChangeNegative = volumeChange < 0;
-    const isSalesChangeNegative = salesChange < 0;
+  const isFloorChangeNegative = floorChange < 0;
+  const isVolumeChangeNegative = volumeChange < 0;
+  const isSalesChangeNegative = salesChange < 0;
 
-    return (
-        <>
-            <Separator />
+  return (
+    <>
+      <Separator />
 
-            <Link href={collectionHref} className="grid grid-cols-10 items-center py-4 transition-background hover:bg-gray-200 dark:hover:bg-gray-900">
-                <div className="flex items-center gap-4 col-span-2">
-                    <Image src={collectionLogoSrc} width={50} height={50} alt="logo of a collection" className="rounded-lg" />
-                    <p>{collectionName}</p>
-                    {isVerified && <Image src="/assets/verify.png" width={20} height={20} alt="verified logo" />}
-                </div>
+      <Link
+        href={collectionHref}
+        className="grid grid-cols-10 items-center py-4 transition-background hover:bg-gray-200 dark:hover:bg-gray-900"
+      >
+        <div className="col-span-2 flex items-center gap-4">
+          <Image
+            src={collectionLogoSrc}
+            width={50}
+            height={50}
+            alt="logo of a collection"
+            className="rounded-lg"
+          />
+          <p>{collectionName}</p>
+          {isVerified && (
+            <Image
+              src="/assets/verify.png"
+              width={20}
+              height={20}
+              alt="verified logo"
+            />
+          )}
+        </div>
 
-                <p className="text-center">{`${floorPrice} MATIC`}</p>
-                <p className={cn("text-center text-green", { "text-red-600": isFloorChangeNegative })}>
-                    {!isFloorChangeNegative && "+"}{floorChange}%</p>
+        <p className="text-center">{`${floorPrice} MATIC`}</p>
+        <p
+          className={cn("text-center text-green", {
+            "text-red-600": isFloorChangeNegative,
+          })}
+        >
+          {!isFloorChangeNegative && "+"}
+          {floorChange}%
+        </p>
 
-                <p className="text-center">{`${volume} MATIC`}</p>
-                <p className={cn("text-center text-green", { "text-red-600": isVolumeChangeNegative })}>
-                    {!isVolumeChangeNegative && "+"}{volumeChange}%</p>
+        <p className="text-center">{`${volume} MATIC`}</p>
+        <p
+          className={cn("text-center text-green", {
+            "text-red-600": isVolumeChangeNegative,
+          })}
+        >
+          {!isVolumeChangeNegative && "+"}
+          {volumeChange}%
+        </p>
 
-                <p className="text-center">{sales}</p>
-                <p className={cn("text-center text-green", { "text-red-600": isSalesChangeNegative })}>
-                    {!isSalesChangeNegative && "+"}{salesChange}%</p>
+        <p className="text-center">{sales}</p>
+        <p
+          className={cn("text-center text-green", {
+            "text-red-600": isSalesChangeNegative,
+          })}
+        >
+          {!isSalesChangeNegative && "+"}
+          {salesChange}%
+        </p>
 
-                <p className="text-right col-span-2">
-                    {`${allCurrentListedNFTs} of ${allCurrentNFTS} `}
-                    <span className="text-gray-500">({((allCurrentListedNFTs / allCurrentNFTS) * 100).toFixed(2)}%)</span>
-                </p>
-            </Link>
-        </>
-    )
+        <p className="col-span-2 text-right">
+          {`${allCurrentListedNFTs} of ${allCurrentNFTS} `}
+          <span className="text-gray-500">
+            ({((allCurrentListedNFTs / allCurrentNFTS) * 100).toFixed(2)}%)
+          </span>
+        </p>
+      </Link>
+    </>
+  );
 }
--- src/app/collections/page.tsx
+++ src/app/collections/page.tsx
@@ -1,9 +1,34 @@
-import { Separator } from "@/components/ui/separator";
+"use client";
+
 import CollectionRows from "./_components/CollectionRows";
 import DropdownButton from "./_components/DropdownButton";
 import PaginationUI from "./_components/PaginationUI";
+import { useEffect, useState } from "react";
+import { faker } from "@faker-js/faker";
 
 export default function Page() {
+  const [data, setData] = useState<{ image: string; isVerified: boolean }[]>(
+    [],
+  );
+
+  function generateFakeData() {
+    const newImage = faker.image.urlPicsumPhotos();
+    const fakerBoolean = faker.datatype.boolean();
+    return { image: newImage, isVerified: fakerBoolean };
+  }
+
+  useEffect(() => {
+    const storedData = localStorage.getItem("fakerData");
+
+    if (storedData) {
+      setData(JSON.parse(storedData));
+    } else {
+      const newData = Array.from({ length: 15 }, generateFakeData);
+      setData(newData);
+      localStorage.setItem("fakerData", JSON.stringify(newData));
+    }
+  }, []);
+
   return (
     <div>
       <h1 className="mb-10 text-4xl font-bold">Collections</h1>
@@ -22,76 +47,57 @@ export default function Page() {
       </div>
 
       {/* Collections Rows */}
-      <CollectionRows
-        collectionHref="#"
-        collectionLogoSrc="/assets/ex1.png"
-        collectionName="Mutant Ape Yacht"
-        isVerified={true}
-        floorPrice={5.25}
-        floorChange={-2.9}
-        volume={1457.8}
-        volumeChange={68.1}
-        sales={273}
-        salesChange={68.52}
-        allCurrentListedNFTs={667}
-        allCurrentNFTS={19486}
-      />
-      <CollectionRows
-        collectionHref="#"
-        collectionLogoSrc="/assets/ex1.png"
-        collectionName="Mutant Ape Yacht"
-        isVerified={true}
-        floorPrice={5.25}
-        floorChange={-2.9}
-        volume={1457.8}
-        volumeChange={68.1}
-        sales={273}
-        salesChange={68.52}
-        allCurrentListedNFTs={667}
-        allCurrentNFTS={19486}
-      />
-      <CollectionRows
-        collectionHref="#"
-        collectionLogoSrc="/assets/ex1.png"
-        collectionName="Mutant Ape Yacht"
-        isVerified={true}
-        floorPrice={5.25}
-        floorChange={-2.9}
-        volume={1457.8}
-        volumeChange={68.1}
-        sales={273}
-        salesChange={68.52}
-        allCurrentListedNFTs={667}
-        allCurrentNFTS={19486}
-      />
-      <CollectionRows
-        collectionHref="#"
-        collectionLogoSrc="/assets/ex1.png"
-        collectionName="Mutant Ape Yacht"
-        isVerified={true}
-        floorPrice={5.25}
-        floorChange={-2.9}
-        volume={1457.8}
-        volumeChange={68.1}
-        sales={273}
-        salesChange={68.52}
-        allCurrentListedNFTs={667}
-        allCurrentNFTS={19486}
-      />
-      <CollectionRows
-        collectionHref="#"
-        collectionLogoSrc="/assets/ex1.png"
-        collectionName="Mutant Ape Yacht"
-        isVerified={true}
-        floorPrice={5.25}
-        floorChange={-2.9}
-        volume={1457.8}
-        volumeChange={68.1}
-        sales={273}
-        salesChange={68.52}
-        allCurrentListedNFTs={667}
-        allCurrentNFTS={19486}
-      />
+      {data.map((item, i) => {
+        return (
+          <CollectionRows
+            collectionHref="#"
+            collectionLogoSrc={item.image}
+            collectionName="Sample Name"
+            isVerified={item.isVerified}
+            floorPrice={faker.number.float({
+              min: 1,
+              max: 10,
+              fractionDigits: 2,
+            })}
+            floorChange={faker.number.float({
+              min: -5,
+              max: 5,
+              fractionDigits: 2,
+            })}
+            volume={faker.number.float({
+              min: 100,
+              max: 2000,
+              fractionDigits: 2,
+            })}
+            volumeChange={faker.number.float({
+              min: -70,
+              max: 70,
+              fractionDigits: 2,
+            })}
+            sales={faker.number.float({
+              min: 10,
+              max: 500,
+              fractionDigits: 0,
+            })}
+            salesChange={faker.number.float({
+              min: -70,
+              max: 70,
+              fractionDigits: 2,
+            })}
+            allCurrentListedNFTs={faker.number.float({
+              min: 200,
+              max: 800,
+              fractionDigits: 0,
+            })}
+            allCurrentNFTS={faker.number.float({
+              min: 3000,
+              max: 20000,
+              fractionDigits: 0,
+            })}
+            key={i}
+          />
+        );
+      })}
 
       <PaginationUI />
     </div>
