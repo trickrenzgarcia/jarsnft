c4d2db9b6206746792b873e0ce1f528458cd4717
Rigor Syguat <rigorsyguatbsn@gmail.com>
2024-05-26 17:53:37 +0800

    change learn navbar style

--- src/app/insights/_components/LearnNavbar.tsx
+++ src/app/insights/_components/LearnNavbar.tsx
@@ -10,9 +10,9 @@ export default function LearnNavbar() {
   const router = useRouter();
 
   return (
-    <header className="sticky top-0 z-50 w-full border-b border-gray-600 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-14 max-w-screen-2xl items-center justify-between">
-        <div className="flex items-center ">
+    <header className="sticky top-0 z-50 w-full bg-background/95 py-4 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="container flex items-center justify-between px-20">
+        <div className="flex items-center">
           <Image
             src="/assets/Jarsu.png"
             width={50}
