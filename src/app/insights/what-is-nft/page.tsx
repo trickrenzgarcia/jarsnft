ba45b23a80eadd597d4dcbd45e67deac329bf724
Jepz Dapar <106528202+HeyJepz@users.noreply.github.com>
2024-05-27 18:30:36 +0800

    small change

--- src/app/insights/what-is-nft/page.tsx
+++ src/app/insights/what-is-nft/page.tsx
@@ -27,12 +27,18 @@ const page = () => {
         {`) The most popular blockchains are distributed across many nodes, which is why you’ll hear them described as
           “decentralized.`}
       </P>
-      <P>{`NFTs are unique digital collectibles like artwork, music, or videos. 
+      <P>
+        {`NFTs are unique digital collectibles like artwork, music, or videos. 
           You can buy and sell them online by often using cryptocurrency. 
           Even though they've been around for a while, NFTs are becoming more popular, especially for digital art. 
           The market for NFTs is huge and is close to being as valuable as the traditional art market. 
           One of the key things that makes NFTs valuable is that they are rare or one-of-a-kind, unlike most digital things which can be easily copied. 
-          This rarity could make them more valuable, similar to other collectible items.`}</P>
+          This rarity could make them more valuable, similar to other collectible items. (`}
+        <span className="font-semibold text-[#A519D7] underline ">
+          <a href="/insights/what-makes-nft-valuable">Learn more here</a>
+        </span>
+        {")"}
+      </P>
       <h2 className="my-6 text-2xl font-bold">
         How is an NFT different from a Cryptocurrency?
       </h2>
