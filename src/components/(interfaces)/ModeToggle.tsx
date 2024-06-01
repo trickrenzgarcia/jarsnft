cd298db344cb7aebe4799deb5000cdf5aba27c20
Patrick <trickrenzgarcia@gmail.com>
2024-06-02 03:18:47 +0800

    fix hydration prerendering

--- src/components/(interfaces)/ModeToggle.tsx
+++ src/components/(interfaces)/ModeToggle.tsx
@@ -1,29 +1,46 @@
 "use client";
 
-import { Moon, Sun } from "lucide-react";
+import dynamic from 'next/dynamic'
+import { useTheme } from "next-themes";
 
-type ModeToggleProps = {
-  setTheme: (theme: string) => void;
-  theme: string | undefined;
-};
-export default function ModeToggle({ setTheme, theme }: ModeToggleProps) {
+const LuMoon = dynamic(() => import("react-icons/lu").then((mod) => mod.LuMoon), { ssr: false });
+const LuSun = dynamic(() => import("react-icons/lu").then((mod) => mod.LuSun), { ssr: false });
+
+export default function ModeToggle() {
+  const { theme, setTheme } = useTheme();
+
   return (
-    <div className="h-[50px] w-[50px] cursor-pointer rounded-[10px] bg-gray-200 text-2xl dark:bg-card">
-      {theme === "dark" ? (
-        <div
-          className="flex h-full w-full items-center justify-center"
-          onClick={() => setTheme("light")}
-        >
-          <Moon className="h-6 w-6" />
-        </div>
-      ) : (
-        <div
-          className="flex h-full w-full items-center justify-center"
-          onClick={() => setTheme("dark")}
-        >
-          <Sun className="h-6 w-6" />
-        </div>
-      )}
+    <div className="
+      h-[50px] w-[50px] flex justify-center items-center cursor-pointer rounded-[10px] 
+      bg-gray-200 dark:bg-card"
+      onClick={(e) => {
+        e.preventDefault();
+        theme === "dark" ? setTheme("light") : setTheme("dark")}
+      }
+    >
+      {theme === "dark" ? 
+      <LuSun className="text-xl" suppressHydrationWarning /> 
+      :
+      <LuMoon className="text-xl" suppressHydrationWarning />
+      }
+      
     </div>
+    // <div className="h-[50px] w-[50px] cursor-pointer rounded-[10px] bg-gray-200 text-2xl dark:bg-card">
+    //   {theme === "dark" ? (
+    //     <div
+    //       className="flex h-full w-full items-center justify-center"
+    //       onClick={() => setTheme("light")}
+    //     >
+    //       {/* <Moon className="h-6 w-6" /> */}
+    //     </div>
+    //   ) : (
+    //     <div
+    //       className="flex h-full w-full items-center justify-center"
+    //       onClick={() => setTheme("dark")}
+    //     >
+    //       {/* <Sun className="h-6 w-6" /> */}
+    //     </div>
+    //   )}
+    // </div>
   );
 }
--- src/components/(interfaces)/SocialLinkButtons.tsx
+++ src/components/(interfaces)/SocialLinkButtons.tsx
@@ -7,47 +7,36 @@ import {
   FaInstagram,
   FaGithub,
 } from "react-icons/fa";
-import { IconType } from "react-icons";
-import { useTheme } from "next-themes";
-import ModeToggle from "../(interfaces)/ModeToggle";
-import Link from "next/link";
+import SocialLinkButton from "./SocialLinkButton";
+import ModeToggle from "./ModeToggle";
 
 export default function SocialLinkButtons() {
-  const { theme, setTheme } = useTheme();
-
+  
   return (
-    <div className="mb-24">
+    <div className="mb-24" suppressHydrationWarning>
       <h1 className="mb-3 text-center text-2xl font-semibold md:text-left">
         Join us
       </h1>
       <div className="flex flex-col flex-wrap items-center gap-8 md:flex-row md:justify-between md:gap-0">
         <div className="flex gap-2">
-          <SocialLinkButton Icon={FaFacebookF} link="" />
+          <SocialLinkButton Icon={<FaFacebookF />} link="" />
           <SocialLinkButton
-            Icon={FaDiscord}
+            Icon={<FaDiscord />}
             link="https://discord.gg/scBduTZyGd"
           />
-          <SocialLinkButton Icon={FaTwitter} link="" />
-          <SocialLinkButton Icon={FaInstagram} link="" />
+          <SocialLinkButton Icon={<FaTwitter />} link="" />
+          <SocialLinkButton Icon={<FaInstagram />} link="" />
           <SocialLinkButton
-            Icon={FaGithub}
+            Icon={<FaGithub />}
             link="https://github.com/BroJavaDevs"
           />
         </div>
         <div className="">
-          <ModeToggle setTheme={setTheme} theme={theme} />
+          <ModeToggle />
         </div>
       </div>
     </div>
   );
 }
 
-function SocialLinkButton({ Icon, link }: { Icon: IconType; link: string }) {
-  return (
-    <Link href={link} target="_blank">
-      <div className="flex h-[50px] w-[50px] items-center justify-center rounded-[10px] bg-gray-200 text-2xl dark:bg-card">
-        <Icon />
-      </div>
-    </Link>
-  );
-}
+
