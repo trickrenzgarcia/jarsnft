de65040049040e9fd44d9992c5b1af930986173b
Jeffrey Dapar <jeffreydapar@gmail.com>
2024-06-04 12:01:05 +0800

    to be continued

--- src/app/insights/nft-safety/page.tsx
+++ src/app/insights/nft-safety/page.tsx
@@ -37,23 +37,40 @@ export default function NFTSafety() {
         <h4 className="my-6 text-lg font-bold tracking-wider">
         Attacks by Phishing
         </h4>
-        <P>{` Phishing is the practice of impersonating a reliable source in order to deceive consumers into disclosing their passwords, private keys, or other sensitive information. Attackers may design fake websites or send shady emails that impersonate credible NFT wallet providers or marketplaces.`}</P>
+        <P>{`Phishing is the practice of impersonating a reliable source in order to deceive consumers into disclosing their passwords, private keys, or other sensitive information. Attackers may design fake websites or send shady emails that impersonate credible NFT wallet providers or marketplaces.`}</P>
+        
         <h4 className="my-6 text-lg font-bold tracking-wider">
         Malware and Keyloggers
-        </h4>
-        <P>{` On a user's device, malware can be installed with the intention of stealing passwords or private keys. Malware called keyloggers records keystrokes on a user's device and may be able to obtain login passwords for NFT wallets and accounts.`}</P>
+        </h4> 
+        <P>{`On a user's device, malware can be installed with the intention of stealing passwords or private keys. Malware called keyloggers records keystrokes on a user's device and may be able to obtain login passwords for NFT wallets and accounts.`}</P>
+        <P>{`Here are the preventive measures for such attack:`}</P>
+        <ul className="-mt-2 ml-14 list-disc leading-loose tracking-wide dark:text-gray-300">
+          <li className="mb-3">
+            <span className="font-bold">Security and Transparency:</span>
+            {`Unlike traditional systems in which trust is based on a central authority, blockchain generates a tamper-proof public record. Everyone on the network has a copy, making it extremely difficult to modify or cheat. This transparency promotes trust and security in transactions.`}
+          </li>
+
+          <li className="mb-3">
+            <span className="font-bold">Efficiency:</span>
+            {`Blockchain eliminates the middleman. Transactions can be completed directly between parties, eliminating the need for a bank or other institution to verify them. This can save both time and money.`}
+          </li>
+          <li className="mb-6">
+            <span className="font-bold">Reduced Errors:</span>
+            {`Errors are reduced because everyone has the same record, leaving less room for mistakes or inaccuracies when information is copied or transferred between systems..`}
+          </li>
+        </ul>
         <h4 className="my-6 text-lg font-bold tracking-wider">
         Smart Contract Vulnerabilities
         </h4>
-        <P>{` If they are not carefully examined, smart contracts—which make it easier to create and transfer NFTs—may have weaknesses. These weaknesses can be used by hackers to manipulate transactions or steal away money.`}</P>
+        <P>{`If they are not carefully examined, smart contracts—which make it easier to create and transfer NFTs—may have weaknesses. These weaknesses can be used by hackers to manipulate transactions or steal away money.`}</P>
         <h4 className="my-6 text-lg font-bold tracking-wider">
         Social Engineering
         </h4>
-        <P>{` Social engineering attacks involve tricking people into performing actions or disclosing sensitive information. This could include persuading someone to send an NFT to an address controlled by the attacker or revealing their wallet's private key.`}</P>
+        <P>{`Social engineering attacks involve tricking people into performing actions or disclosing sensitive information. This could include persuading someone to send an NFT to an address controlled by the attacker or revealing their wallet's private key.`}</P>
         <h4 className="my-6 text-lg font-bold tracking-wider">
         DNS Spoofing
         </h4>
-        <P>{` DNS spoofing refers to redirecting traffic from a legitimate website to a malicious one. Users attempting to access a legitimate NFT marketplace may be redirected to a fraudulent site, where their login credentials or other sensitive information may be compromised.`}</P>
+        <P>{`DNS spoofing refers to redirecting traffic from a legitimate website to a malicious one. Users attempting to access a legitimate NFT marketplace may be redirected to a fraudulent site, where their login credentials or other sensitive information may be compromised.`}</P>
       </div>
       <P>{`Check out how you can protect your wallet and account in your web3 journey.`}</P>
       <span className="font-semibold text-[#A519D7] underline ">
