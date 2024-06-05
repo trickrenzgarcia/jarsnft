f3d2f6e412a9ac8c6589c95001e5ffaa6e84269c
Patrick <trickrenzgarcia@gmail.com>
2024-06-05 21:22:10 +0800

    gumana na

---
+++ src/app/api/nft_views/route.ts
@@ -0,0 +1,23 @@
+import { jars } from "@/lib/core/api";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+const viewSchema = z.object({
+  contract: z.string().min(1),
+  tokenId: z.string().min(1),
+});
+
+export async function GET(req: NextRequest) {
+  const contract = req.nextUrl.searchParams.get("contract");
+  const tokenId = req.nextUrl.searchParams.get("token_id");
+
+  const views = viewSchema.safeParse({ contract, tokenId });
+
+  if(!views.success) {
+    return NextResponse.json({ error: "Invalid Parameters" }, { status: 400, statusText: "Bad Request"});
+  }
+
+  const data = await jars.nft.getNftViews(views.data.contract, views.data.tokenId);
+
+  return NextResponse.json({ data });
+}
