24900940ce129d23bccc4ad108f4ff6897c79000
Rigor Syguat <rigorsyguatbsn@gmail.com>
2024-06-09 16:19:12 +0800

    make text bigger

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -17,16 +17,22 @@ export default function CreatePage() {
           {/* Create NFT Container */}
           <div className="flex w-auto flex-col justify-center gap-6 p-0 md:w-[50svw] md:p-6">
             {/* Medium screen and up Title */}
-            <h1 className="animate-once animate-duration-[1200ms] animate-ease-out relative mb-6 max-w-4xl animate-fade-up text-center text-5xl font-bold text-zinc-700 dark:text-zinc-100 md:flex md:text-left md:text-7xl">
-              Make your art, NFT
+            <h1
+              className="animate-once animate-duration-[1200ms] animate-ease-out relative mb-6 max-w-4xl animate-fade-up text-center text-6xl font-bold text-zinc-700 dark:text-zinc-100 md:flex md:text-left md:text-7xl"
+              style={{ whiteSpace: "preserve-spaces" }}
+            >
+              {`Make your art, `}
+              <span className="contents text-purple-700">NFT</span>
             </h1>
             <Link
               href="/create/deploy-contract"
               className="mx-4 w-fit md:mx-0 md:w-[38svw]"
             >
-              <Alert className="animate-once animate-duration-[1200ms] animate-ease-out animate-flip-up p-8 hover:border-blue-400 hover:bg-blue-500 hover:shadow-lg hover:shadow-blue-500/50">
-                <AlertTitle>Create NFT Collection</AlertTitle>
-                <AlertDescription>
+              <Alert className="animate-once animate-duration-[1200ms] animate-ease-out animate-flip-up p-6 hover:border-blue-400 hover:bg-blue-500 hover:shadow-lg hover:shadow-blue-500/50">
+                <AlertTitle className="text-lg md:text-xl">
+                  Create NFT Collection
+                </AlertTitle>
+                <AlertDescription className="text-base md:text-lg">
                   The NFT Collection contract is suitable for when you want to
                   have a collection of unique NFTs.
                 </AlertDescription>
@@ -36,9 +42,11 @@ export default function CreatePage() {
               href="/create/mint"
               className="mx-4 w-fit md:mx-0 md:w-[38svw]"
             >
-              <Alert className="animate-once animate-duration-[1200ms] animate-ease-out animate-flip-up cursor-pointer p-8 hover:border-indigo-400 hover:bg-indigo-500 hover:shadow-lg hover:shadow-indigo-500/50">
-                <AlertTitle>Mint an NFT</AlertTitle>
-                <AlertDescription>
+              <Alert className="animate-once animate-duration-[1200ms] animate-ease-out animate-flip-up cursor-pointer p-6 hover:border-indigo-400 hover:bg-indigo-500 hover:shadow-lg hover:shadow-indigo-500/50">
+                <AlertTitle className="text-lg md:text-xl">
+                  Mint an NFT
+                </AlertTitle>
+                <AlertDescription className="text-base md:text-lg">
                   The NFT Mint contract is suitable for when you want to mint a
                   single NFT or a small batch of NFTs.
                 </AlertDescription>
