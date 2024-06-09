1e49e34bcdee8aabdec77740727c829c62862475
Rigor Syguat <rigorsyguatbsn@gmail.com>
2024-06-09 15:56:20 +0800

    fix responsive view

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -13,22 +13,16 @@ export default function CreatePage() {
           <WavyEffect />
         </div>
         {/* Flex container */}
-        <div className="z-10 flex h-[calc(100vh-57px)] w-full items-start justify-around md:container md:items-center">
+        <div className="z-10 flex h-[calc(100vh-57px)] w-full flex-col items-start justify-center md:container md:flex-row md:items-center md:justify-around">
           {/* Create NFT Container */}
-          <div className="flex w-[50svw] flex-col justify-center gap-6 p-0 md:p-6">
-            {/* Art images for small screen */}
-            <div className="flex flex-col md:hidden">
-              <OnBoardingCarouselSm />
-              <h1 className="m-4 text-2xl font-bold">Create NFT Collection</h1>
-            </div>
-
+          <div className="flex w-auto flex-col justify-center gap-6 p-0 md:w-[50svw] md:p-6">
             {/* Medium screen and up Title */}
-            <h1 className="animate-once animate-duration-[1200ms] animate-ease-out relative mb-6 hidden max-w-4xl animate-fade-up text-left text-3xl font-bold text-zinc-700 dark:text-zinc-100 md:flex md:text-7xl">
-              Create NFT Collection
+            <h1 className="animate-once animate-duration-[1200ms] animate-ease-out relative mb-6 max-w-4xl animate-fade-up text-center text-5xl font-bold text-zinc-700 dark:text-zinc-100 md:flex md:text-left md:text-7xl">
+              Make your art, NFT
             </h1>
             <Link
               href="/create/deploy-contract"
-              className="mx-4 w-[38svw] md:mx-0"
+              className="mx-4 w-fit md:mx-0 md:w-[38svw]"
             >
               <Alert className="animate-once animate-duration-[1200ms] animate-ease-out animate-flip-up p-8 hover:border-blue-400 hover:bg-blue-500 hover:shadow-lg hover:shadow-blue-500/50">
                 <AlertTitle>Create NFT Collection</AlertTitle>
@@ -38,7 +32,10 @@ export default function CreatePage() {
                 </AlertDescription>
               </Alert>
             </Link>
-            <Link href="/create/mint" className="mx-4 w-[38svw] md:mx-0">
+            <Link
+              href="/create/mint"
+              className="mx-4 w-fit md:mx-0 md:w-[38svw]"
+            >
               <Alert className="animate-once animate-duration-[1200ms] animate-ease-out animate-flip-up cursor-pointer p-8 hover:border-indigo-400 hover:bg-indigo-500 hover:shadow-lg hover:shadow-indigo-500/50">
                 <AlertTitle>Mint an NFT</AlertTitle>
                 <AlertDescription>
