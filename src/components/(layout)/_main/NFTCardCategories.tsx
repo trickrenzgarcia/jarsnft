68c3872e278bd7e4214d6dd2a9b12276a5fd9338
rigs <rigorsyguatbsn@gmail.com>
2024-08-23 21:51:26 +0800

    change explore nft categories style

--- src/components/(layout)/_main/NFTCardCategories.tsx
+++ src/components/(layout)/_main/NFTCardCategories.tsx
@@ -10,16 +10,44 @@ interface NFTCardCategoriesProps {
 
 export default function NFTCardCategories({ categoryLink, categoryName, image }: NFTCardCategoriesProps) {
   return (
-    <Card className="mx-auto h-[300px] w-80">
-      <CardHeader className="relative h-52 overflow-hidden">
-        <Link href={categoryLink}>
-          <Image className="rounded-[--radius]" src={image} fill alt="NFT Category" style={{ objectFit: "cover" }} />
-        </Link>
-      </CardHeader>
+    // <Card className="mx-auto h-[300px] w-80">
+    //   <CardHeader className="relative h-52 overflow-hidden">
+    //     <Link href={categoryLink}>
+    //       <Image className="rounded-[--radius]" src={image} fill alt="NFT Category" style={{ objectFit: "cover" }} />
+    //     </Link>
+    //   </CardHeader>
 
-      <CardFooter className="flex h-[5.5rem] items-start p-0 pl-6">
-        <p className="my-auto">{categoryName}</p>
-      </CardFooter>
-    </Card>
+    //   <CardFooter className="flex h-[5.5rem] items-start p-0 pl-6">
+    //     <p className="my-auto">{categoryName}</p>
+    //   </CardFooter>
+    // </Card>
+    <Link href={categoryLink}>
+      <Card className="cursor-pointer rounded-lg border-2 bg-background p-2 hover:-translate-y-1 hover:border-3 hover:border-zinc-200 dark:hover:border-zinc-800">
+        <CardHeader className="flex aspect-square items-center justify-center p-0">
+          <Image
+            src={image}
+            alt={"Image for " + categoryName}
+            loading="eager"
+            width={300}
+            height={300}
+            style={{
+              height: "300px",
+              width: "300px",
+              objectFit: "cover",
+              objectPosition: "center",
+            }}
+            className="h-full w-full rounded-md"
+          />
+        </CardHeader>
+        <CardContent className="mt-1 pl-1">
+          <CardDescription className="flex items-center gap-1 text-xs">
+            <span className="truncate">{categoryName}</span>
+          </CardDescription>
+          <CardTitle className="flex items-center text-sm">
+            <span className="truncate">{categoryName}</span>
+          </CardTitle>
+        </CardContent>
+      </Card>
+    </Link>
   );
 }
--- src/components/(layout)/_main/NFTCategories.tsx
+++ src/components/(layout)/_main/NFTCategories.tsx
@@ -3,10 +3,9 @@ import NFTCardCategories from "./NFTCardCategories";
 export default function NFTCategories() {
   return (
     <>
-      {/* <div className="flex flex-col gap-2 md:mx-6 md:grid md:grid-cols-3"> */}
       <div className="container mx-auto p-4">
         <p className="mb-12 text-center text-2xl font-bold lg:text-3xl">Explore NFT Categories</p>
-        <div className="flex flex-wrap justify-center gap-6 overflow-x-auto md:flex-nowrap">
+        <div className="flex flex-wrap justify-center gap-12 overflow-x-auto p-2 md:flex-nowrap">
           <NFTCardCategories categoryLink="/category/arts" categoryName="Art NFTs" image="/assets/categoryImages/ArtCategory.png" />
           <NFTCardCategories categoryLink="/category/photography" categoryName="Photography NFTs" image="/assets/categoryImages/PhotoCategory.png" />
           <NFTCardCategories categoryLink="/category/pfp" categoryName="Profile Picture NFTs" image="/assets/categoryImages/PFPCategory.png" />
