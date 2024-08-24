f30d7a5c888ee74a5160d9c50c213b77d8fcf550
rigs <rigorsyguatbsn@gmail.com>
2024-08-24 23:42:54 +0800

    change margin

--- src/app/(routes)/collections/page.tsx
+++ src/app/(routes)/collections/page.tsx
@@ -1,14 +1,12 @@
 import CollectionData from "./_components/CollectionData";
 import DropdownControls from "./_components/DropdownControls";
 
-export default function CollectionsPage({
-  searchParams,
-}: {
-  searchParams: { [key: string]: string | string[] | undefined };
-}) {
+export default function CollectionsPage({ searchParams }: { searchParams: { [key: string]: string | string[] | undefined } }) {
   return (
     <>
-      <h1 className="animate-flip-down animate-once animate-duration-[1200ms] animate-ease-in my-2 mx-auto font-bold text-3xl lg:text-7xl">Collections</h1>
+      <h1 className="animate-once animate-duration-[1200ms] animate-ease-in mx-auto mb-6 mt-4 animate-flip-down text-3xl font-bold lg:text-7xl">
+        Collections
+      </h1>
       <DropdownControls searchParams={searchParams} />
       <CollectionData searchParams={searchParams} />
     </>
