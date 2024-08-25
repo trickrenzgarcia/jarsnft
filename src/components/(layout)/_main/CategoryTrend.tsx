614e33940d616e71a8eff204facf962212f37a24
AKCcoding <alraeatwork@gmail.com>
2024-08-25 18:54:10 +0800

    Fix button on light/dark mode

--- src/components/(layout)/_main/CategoryTrend.tsx
+++ src/components/(layout)/_main/CategoryTrend.tsx
@@ -6,7 +6,7 @@ export default function CategoryTrend({ name, link }: any) {
     <div className="flex flex-col lg:flex-row justify-between gap-y-2 my-2">
       <h1 className="text-center content-center self-center text-xl lg:text-5xl font-bold">Trending in {name}</h1>
       <div></div>
-      <Button variant="ghost" className="lg:text-md leading-md rounded-xl bg-[#1b1b1b] text-sm font-bold hover:bg-[#252525]">
+      <Button className="lg:text-md leading-md rounded-xl text-sm font-bold">
         <Link href={link}>View {name}</Link>
       </Button>
     </div>
