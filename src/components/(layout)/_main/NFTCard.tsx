c39451d64a7e0c5235cdb5b8bf468ec5c0972b66
rigs <rigorsyguatbsn@gmail.com>
2024-09-30 18:10:21 +0800

    fix link missing

--- src/components/(layout)/_main/NFTCard.tsx
+++ src/components/(layout)/_main/NFTCard.tsx
@@ -25,45 +25,49 @@ import { MdVerified } from "react-icons/md";
 
 export default function NFTCard({ item }: { item: CollectionData }) {
   return (
-    <Card className="p-2 border-2 bg-background rounded-lg hover:-translate-y-1 cursor-pointer hover:border-3 hover:border-zinc-200 dark:hover:border-zinc-800">
-      <CardHeader className='flex items-center justify-center aspect-square p-0'>
-        <Image
-          src={item.image}
-          alt={item.name}
-          width={300}
-          height={300}
-          loading='eager'
-          style={{
-            objectFit: 'cover',
-            objectPosition: 'center'
-          }}
-          className="h-full w-full rounded-md"
-        />
-      </CardHeader>
+    <Link href={`/collection/${item.contract}`}>
+      <Card className="p-2 border-2 bg-background rounded-lg hover:-translate-y-1 cursor-pointer hover:border-3 hover:border-zinc-200 dark:hover:border-zinc-800">
+        <CardHeader className='flex items-center justify-center aspect-square p-0'>
+          <Image
+            src={item.image}
+            alt={item.name}
+            width={300}
+            height={300}
+            loading='eager'
+            style={{
+              objectFit: 'cover',
+              objectPosition: 'center'
+            }}
+            className="h-full w-full rounded-md"
+          />
+        </CardHeader>
 
-      <CardContent className='mt-1 pl-1'>
-        <CardDescription className='flex items-center gap-1 text-xs'>
-          <span className='truncate'>{item.name}</span>
-          {item.isVerified && (<MdVerified className='text-xs' />)}
-        </CardDescription>
-        <CardTitle className='flex items-center text-sm'>
-          <span className='truncate'>{item.name}</span>
-          <span>&nbsp;{item.symbol && `(${item.symbol})`}</span>
-        </CardTitle>
-      </CardContent>
+        <CardContent className='mt-1 pl-1'>
+          <CardDescription className='flex items-center gap-1 text-xs'>
+            <span className='truncate'>{item.name}</span>
+            {item.isVerified && (<MdVerified className='text-xs' />)}
+          </CardDescription>
+          <CardTitle className='flex items-center text-sm'>
+            <span className='truncate'>{item.name}</span>
+            <span>&nbsp;{item.symbol && `(${item.symbol})`}</span>
+          </CardTitle>
+        </CardContent>
 
-      <CardFooter className={cn('w-full mt-2 flex justify-between rounded-lg p-3 bg-muted dark:bg-muted/30')}>
-        <div className='flex flex-col gap-1'>
-          <p className='text-xs truncate text-muted-foreground'>Floor</p>
-          <p className='text-sm font-semibold truncate'>100 MATIC</p>
-        </div>
-        <div className='flex flex-col gap-1'>
-          <p className='text-xs truncate text-muted-foreground'>Volume</p>
-          <p className='text-sm font-semibold truncate'>100 MATIC</p>
-        </div>
-      </CardFooter>
+        <CardFooter className={cn('w-full mt-2 flex justify-between rounded-lg p-3 bg-muted dark:bg-muted/30')}>
+          <div className='flex flex-col gap-1'>
+            <p className='text-xs truncate text-muted-foreground'>Floor</p>
+            <p className='text-sm font-semibold truncate'>100 MATIC</p>
+          </div>
+          <div className='flex flex-col gap-1'>
+            <p className='text-xs truncate text-muted-foreground'>Volume</p>
+            <p className='text-sm font-semibold truncate'>100 MATIC</p>
+          </div>
+        </CardFooter>
+      </Card >
+    </Link>
+  );
 
-      {/* <CardHeader className="relative h-80">
+  {/* <CardHeader className="relative h-80">
         <Link href={`/collection/${collectionLink}`}>
           <Image
             src={image}
@@ -83,9 +87,9 @@ export default function NFTCard({ item }: { item: CollectionData }) {
         </Link>
       </CardHeader> */}
 
-      {/* <CardFooter className="mt-10 flex flex-col items-start gap-4"> */}
-      {/* Name & Badge */}
-      {/* <div className="flex gap-2">
+  {/* <CardFooter className="mt-10 flex flex-col items-start gap-4"> */ }
+  {/* Name & Badge */ }
+  {/* <div className="flex gap-2">
           <Link href={`/collection/${collectionLink}`}>
             <p className="font-bold">{name}</p>
           </Link>
@@ -99,8 +103,8 @@ export default function NFTCard({ item }: { item: CollectionData }) {
           )}
         </div> */}
 
-      {/* Statistics */}
-      {/* <div className="flex w-full justify-between">
+  {/* Statistics */ }
+  {/* <div className="flex w-full justify-between">
           <div className="flex flex-col">
             <p className="text-gray-500">Floor</p>
             <p className="font-bold">
@@ -118,6 +122,4 @@ export default function NFTCard({ item }: { item: CollectionData }) {
           </div>
         </div>
       </CardFooter> */}
-    </Card>
-  );
 }
