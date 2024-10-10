ab3c6932698a5b4ff6263ce03eace0e9119116c5
rwick <trickrenzgarcia@gmail.com>
2024-10-10 23:28:32 +0800

    feat: adding PWA

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,25 @@
+import { withPWA } from 'next-pwa';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withPWA({
+  dest: 'public', // This will generate the service worker and other PWA assets in the public folder
   reactStrictMode: false,
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '**'
-      }
-    ]
+        hostname: '**',
+      },
+    ],
   },
   webpack: (config) => {
-    config.externals.push('pino-pretty', 'lokijs', 'encoding')
-    return config
-  }
-};
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    return config;
+  },
+  pwa: {
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development', // Disable PWA in development mode
+  },
+});
 
 export default nextConfig;
