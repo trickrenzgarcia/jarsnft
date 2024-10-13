7f614b533fed606c9a29817efddc44159cba068e
AKCcoding <alraeatwork@gmail.com>
2024-10-13 14:22:36 +0800

    FIX CG API

--- src/app/(routes)/coins/_components/cmc-table/CMCTable.jsx
+++ src/app/(routes)/coins/_components/cmc-table/CMCTable.jsx
@@ -6,10 +6,12 @@ import { MobileTableRow } from "./CMCTableRow";
 import { MobileTableHeader } from "./CMCTableHeader";
 
 const CMCTable = () => {
+  
   const [currency, setCurrency] = useState("usd");
   const { response } = useAxios(
-    `coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=100&page=1&sparkline=true&price_change_percentage=7d&locale=en&x_cg_demo_api_key=${(process.env.NEXT_PUBLIC_COINGECKO_API_KEY)}`,
+    `coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=100&page=1&sparkline=true&price_change_percentage=7d&locale=en&x_cg_demo_api_key=${process.env.NEXT_PUBLIC_CG_API_KEY}`,
   );
+  console.log(process.env.NEXT_PUBLIC_CG_API_KEY);
   // Function to handle currency change
   const CurrencyChange = () => {
     // Toggle between USD and PHP
