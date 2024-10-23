7d35019e3c5c01290e4150c2778837600727c199
rwick <trickrenzgarcia@gmail.com>
2024-10-24 07:47:11 +0800

    Baka magkita pa tayo sa Q.C., sa UP, sa kalsada ng BGC

--- src/app/(routes)/collection/[address]/[id]/_components/index.tsx
+++ src/app/(routes)/collection/[address]/[id]/_components/index.tsx
@@ -7,7 +7,6 @@ export { default as DisplayActivities } from "./DisplayActivities";
 export { default as DisplayBidders } from "./DisplayBidders";
 export { default as EventChip } from "./EventChip";
 export { default as Favorite } from "./Favorite";
-export { default as NftCard } from "./NftCard";
 export { default as NftMetadata } from "./NftMetadata";
 export { default as PlaceBidButton } from "./PlaceBidButton";
 export { default as SellButton } from "./SellButton";
