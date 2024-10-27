3cd5e9024acf43a4c82f3a589e6331ab324774c7
rigs <rigorsyguatbsn@gmail.com>
2024-10-27 22:28:42 +0800

    fix "srcSet did not match" error

--- src/components/(interfaces)/JarsLogo.tsx
+++ src/components/(interfaces)/JarsLogo.tsx
@@ -2,22 +2,24 @@
 
 import { useTheme } from "next-themes";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function JarsLogo() {
   const { theme } = useTheme();
+  const [logo, setLogo]: any = useState();
+
+  useEffect(() => {
+    if (theme === "dark") {
+      setLogo(<Image src="/jars_transparent_dark.png" width={50} height={50} alt="jars" />);
+    }
+    else {
+      setLogo(<Image src="/jars_transparent.png" width={50} height={50} alt="jars" />);
+    }
+  }, [theme]);
 
   return (
     <div className="animate-pulse">
-      {theme === "dark" ? (
-        <Image
-          src="/jars_transparent_dark.png"
-          width={50}
-          height={50}
-          alt="jars"
-        />
-      ) : (
-        <Image src="/jars_transparent.png" width={50} height={50} alt="jars" />
-      )}
+      {logo}
     </div>
   );
 }
