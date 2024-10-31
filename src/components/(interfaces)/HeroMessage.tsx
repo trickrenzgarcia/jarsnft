2de21841cbf795264383e1add70e85ec18952ba1
Jepz Dapar <106528202+HeyJepz@users.noreply.github.com>
2024-10-31 19:49:46 +0800

    style fix

--- src/components/(interfaces)/HeroMessage.tsx
+++ src/components/(interfaces)/HeroMessage.tsx
@@ -31,12 +31,13 @@ export default function HeroMessage() {
   };
 
   // If the component hasn't mounted yet, don't render anything
-  if (!isMounted || hasBeenClosed || isMetaMask) return null;
+  // if (!isMounted || hasBeenClosed || isMetaMask) return null;
 
   return (
     <div className={cn("bg-purple-600 flex gap-4 lg:hidden items-center min-h-8 p-4")}>
-      <h3 className='ml-auto'>
-        We recommend using <span className='font-bold'>Metamask</span>, check our guide
+      <h3 className='ml-auto text-center'>
+        We recommend using <span className='font-bold'>Metamask</span>, 
+        {` check our guide `} 
         <Link href="/insights/setup-wallet" className='font-bold underline'>
           here
         </Link>.
