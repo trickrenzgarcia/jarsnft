028c81968788dae0049fd65a29e4d97a81f957ce
Jepz Dapar <106528202+HeyJepz@users.noreply.github.com>
2024-11-06 20:34:22 +0800

    -modified insights style sidebar to sheet component +FAQs migrate question -modified navbar

--- src/app/(routes)/insights/_components/FAQs.tsx
+++ src/app/(routes)/insights/_components/FAQs.tsx
@@ -8,51 +8,55 @@ import { H3 } from "./TailwindTags";
 
 const faqData = [
   {
-    question: '1. What can be an NFT?',
+    question: 'What can be an NFT?',
     answer: 'NFTs can represent a wide range of digital assets, including artwork, music, videos, virtual real estate, in-game items, and even tweets.'
   },
   {
-    question: '2. What are some common uses for NFTs?',
+    question: 'What are some common uses for NFTs?',
     answer: 'Common uses include digital art, collectibles, virtual real estate, gaming items, music, and ticketing for events. They are also used for verifying the ownership and authenticity of physical assets.'
   },
   {
-    question: '3. Can NFTs be copied or duplicated?',
+    question: 'Can NFTs be copied or duplicated?',
     answer: 'The digital content associated with an NFT can be copied, but the ownership and provenance verified by the blockchain cannot be duplicated. This is akin to owning the original painting versus owning a print.'
   },
   {
-    question: '4. What are the risks associated with NFTs?',
+    question: 'What are the risks associated with NFTs?',
     answer: 'Risks include market volatility, potential loss of access to your digital wallet, legal uncertainties regarding intellectual property, and the possibility of purchasing counterfeit or plagiarized content.'
   },
   {
-    question: '5. Can I lose my NFT?',
+    question: 'Can I lose my NFT?',
     answer: 'Yes, if you lose access to your digital wallet where the NFT is stored, you may lose the NFT. It’s important to securely store your wallet’s private keys and backup phrases.'
   },
   {
-    question: '6. How do royalties work with NFTs?',
+    question: 'Can I migrate my NFTs from a different platform?',
+    answer: 'Yes, ....'
+  },
+  {
+    question: 'How do royalties work with NFTs?',
     answer: 'Creators can set up smart contracts for their NFTs to receive royalties automatically each time their NFT is sold on a secondary market. This allows artists to earn ongoing revenue from their work.'
   },
   {
-    question: '7. What are the risks associated with NFTs?',
+    question: 'What are the risks associated with NFTs?',
     answer: 'Risks include market volatility, potential loss of access to your digital wallet, legal uncertainties regarding intellectual property, and the possibility of purchasing counterfeit or plagiarized content.'
   },
   {
-    question: '8. What is a digital wallet?',
+    question: 'What is a digital wallet?',
     answer: 'A digital wallet is an application that allows you to store, send, and receive digital assets like cryptocurrencies and NFTs. Examples include MetaMask, Trust Wallet, and Coinbase Wallet.'
   },
   {
-    question: '9. What are the key features of blockchain?',
+    question: 'What are the key features of blockchain?',
     answer: 'Blockchain provides decentralization, immutability, transparency, and security, with applications in digital assets like cryptocurrencies and NFTs.'
   },
   {
-    question: '10. What are some common applications of blockchain?',
+    question: 'What are some common applications of blockchain?',
     answer: 'Common applications include cryptocurrencies (Bitcoin, Ethereum), supply chain management, voting systems, identity verification, smart contracts, and NFTs.'
   },
   {
-    question: '11. How is blockchain different from traditional databases?',
+    question: 'How is blockchain different from traditional databases?',
     answer: 'Unlike traditional databases managed by a central authority, blockchain is decentralized and managed by a network of nodes. Blockchains also provide greater transparency and immutability compared to traditional databases.'
   },
   {
-    question: '12. Can blockchain be hacked?',
+    question: 'Can blockchain be hacked?',
     answer: 'While blockchain itself is very secure, individual vulnerabilities such as weak passwords, phishing attacks, and poorly implemented smart contracts can be exploited. The security of the network depends on the collective security of its participants.'
   }
 ];
@@ -65,7 +69,7 @@ export default function FAsInsight() {
       <Accordion type="single" collapsible className="w-full">
       {faqData.map((faq, index) => (
         <AccordionItem key={index} value={`item-${index + 1}`}>
-          <AccordionTrigger>{faq.question}</AccordionTrigger>
+          <AccordionTrigger>{`${index + 1}. ` + faq.question}</AccordionTrigger>
           <AccordionContent>
             <span className="leading-loose tracking-wide dark:text-gray-300">
               {faq.answer}
--- src/app/(routes)/insights/_components/LearnNavbar.tsx
+++ src/app/(routes)/insights/_components/LearnNavbar.tsx
@@ -12,17 +12,20 @@ export default function LearnNavbar() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b border-gray-700 bg-background/95 py-2 backdrop-blur supports-[backdrop-filter]:bg-background/60 md:py-4 ">
-      <div className="container flex items-center justify-center px-4 lg:justify-between lg:px-20">
+      <div className="container flex h-6 md:h-auto items-center justify-center px-4 lg:justify-between lg:px-20">
         {/* flex w-full items-center justify-center md:w-fit md:justify-normal */}
         <div className="-ml-4 flex items-center justify-between lg:w-fit lg:justify-normal">
-          <Image
-            src="/assets/Jarsu.png"
-            width={50}
-            height={50}
-            alt="Insights Logo"
-          />
+          <div className="w-10 md:w-12"> {/* 40px (10) for small screens and 50px (12) for medium and up */}
+            <Image
+              src="/assets/Jarsu.png"
+              width={50} // Keep the original width for the image
+              height={50} // Keep the original height for the image
+              alt="Insights Logo"
+              className="object-contain" // Ensure the image fits within the container
+            />
+          </div>
           <Link href="/">
-            <h1 className="text-3xl">Insights</h1>
+            <h1 className="text-xl md:text-3xl">Insights</h1>
           </Link>
         </div>
         <div className="hidden lg:block">
