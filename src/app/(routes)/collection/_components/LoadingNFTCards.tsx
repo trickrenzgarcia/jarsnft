b2aa1ac721bdb197573639980cb29d8e2dea5601
AKCcoding <alraeatwork@gmail.com>
2024-11-15 13:56:36 +0800

    oops

--- src/app/(routes)/collection/_components/LoadingNFTCards.tsx
+++ src/app/(routes)/collection/_components/LoadingNFTCards.tsx
@@ -4,7 +4,7 @@ export default function LoadingNFTCards() {
   return (
     <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">
       {Array.from({ length: 6 }).map((_, index: number) => (
-        <Skeleton key={index} className="min-h-[375px] min-w-[257px]" />
+        <Skeleton key={index} className="min-h-[175px] min-w-[121px] lg:min-h-[375px] lg:min-w-[257px]" />
       ))}
     </div>
   );
