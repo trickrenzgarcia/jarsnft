a62420fb2451d8e38f7ae713a43e006b35862623
rwick <trickrenzgarcia@gmail.com>
2024-11-16 16:12:24 +0800

    deploy new

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,35 +1,34 @@
-import { Hono } from 'hono'
-import { handle } from 'hono/vercel'
-import { jwt } from 'hono/jwt'
-import * as router from '@/app/api/_routers'
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+import { jwt } from "hono/jwt";
+import * as router from "@/app/api/_routers";
 
-const app = new Hono().basePath('/api/v1')
+const app = new Hono().basePath("/api/v1");
 
 const jwtMiddleware = jwt({
   secret: process.env.JWT_SECRET,
-  alg: 'HS256'
-})
+  alg: "HS256",
+});
 
+app.use("/*", jwtMiddleware);
 
-app.use('/*', jwtMiddleware)
+app.get("/protected", jwtMiddleware, async (c) => {
+  return c.json({ message: "Protected routes" });
+});
 
-app.get('/protected', jwtMiddleware, async (c) => {
-  return c.json({ message: 'Protected routes' })
-})
+app.route("/user", router.user);
+app.route("/collection", router.collection);
+app.route("/collections", router.collections);
+app.route("/contracts", router.contracts);
+app.route("/address", router.address);
+app.route("/nonce", router.nonce);
+app.route("/metadata", router.metadata);
+app.route("/mint", router.mint);
+app.route("/deploy", router.deploy);
+app.route("/nfts", router.nfts);
+app.route("/storage", router.storage);
+app.route("/search", router.search);
+app.route("/test", router.test);
 
-app.route('/user', router.user)
-app.route('/collection', router.collection)
-app.route('/collections', router.collections)
-app.route('/contracts', router.contracts)
-app.route('/address', router.address)
-app.route('/nonce', router.nonce)
-app.route('/metadata', router.metadata)
-app.route('/mint', router.mint)
-app.route('/deploy', router.deploy)
-app.route('/nfts', router.nfts)
-app.route('/storage', router.storage)
-app.route('/search', router.search)
-
-
-export const GET = handle(app)
-export const POST = handle(app)
+export const GET = handle(app);
+export const POST = handle(app);
