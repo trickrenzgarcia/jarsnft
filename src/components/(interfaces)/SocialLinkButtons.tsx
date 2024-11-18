dc589e82aa7cf2831048a97927fac56f411ea74f
AKCcoding <alraeatwork@gmail.com>
2024-11-18 23:34:02 +0800

    Add bot and light/dark mode together

--- src/components/(interfaces)/SocialLinkButtons.tsx
+++ src/components/(interfaces)/SocialLinkButtons.tsx
@@ -1,42 +1,21 @@
 "use client";
 
-import {
-  FaFacebookF,
-  FaDiscord,
-  FaTwitter,
-  FaInstagram,
-  FaGithub,
-} from "react-icons/fa";
+import { FaFacebookF, FaDiscord, FaTwitter, FaInstagram, FaGithub } from "react-icons/fa";
 import SocialLinkButton from "./SocialLinkButton";
-import ModeToggle from "./ModeToggle";
 
 export default function SocialLinkButtons() {
-  
   return (
     <div className="mb-24" suppressHydrationWarning>
-      <h1 className="mb-3 text-center text-2xl font-semibold md:text-left">
-        Join us
-      </h1>
+      <h1 className="mb-3 text-center text-2xl font-semibold md:text-left">Join us</h1>
       <div className="flex flex-col flex-wrap items-center gap-8 md:flex-row md:justify-between md:gap-0">
         <div className="flex gap-2">
           <SocialLinkButton Icon={<FaFacebookF />} link="" />
-          <SocialLinkButton
-            Icon={<FaDiscord />}
-            link="https://discord.gg/scBduTZyGd"
-          />
+          <SocialLinkButton Icon={<FaDiscord />} link="https://discord.gg/scBduTZyGd" />
           <SocialLinkButton Icon={<FaTwitter />} link="" />
           <SocialLinkButton Icon={<FaInstagram />} link="" />
-          <SocialLinkButton
-            Icon={<FaGithub />}
-            link="https://github.com/BroJavaDevs"
-          />
-        </div>
-        <div className="">
-          <ModeToggle />
+          <SocialLinkButton Icon={<FaGithub />} link="https://github.com/BroJavaDevs" />
         </div>
       </div>
     </div>
   );
 }
-
-
--- src/components/(layout)/_main/ChatbotModal.tsx
+++ src/components/(layout)/_main/ChatbotModal.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import { Bot } from "lucide-react";
-import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure } from "@nextui-org/react";
+import { Modal, ModalContent, ModalHeader, ModalBody, useDisclosure } from "@nextui-org/react";
 import Chatbot from "@/app/(routes)/chat/page";
 import { FaRobot } from "react-icons/fa";
 
@@ -11,26 +11,24 @@ export default function ChatbotModal({ className }: { className: string }) {
   return (
     <>
       <div className={className}>
-        <div className="flex flex-col items-baseline self-center">
-          <Button color="primary" onPress={onOpen}>
-            <Bot />
-          </Button>
-          <Modal backdrop="blur" isOpen={isOpen} onOpenChange={onOpenChange} size="5xl" scrollBehavior="inside">
-            <ModalContent>
-              {(onClose) => (
-                <>
-                  <ModalHeader className="flex items-center justify-center gap-2">
-                    <FaRobot className="text-5xl" />
-                    JARS AI Chatbot
-                  </ModalHeader>
-                  <ModalBody>
-                    <Chatbot />
-                  </ModalBody>
-                </>
-              )}
-            </ModalContent>
-          </Modal>
+        <div className="flex h-[50px] w-[50px] cursor-pointer items-center justify-center rounded-[10px] bg-gray-200 dark:bg-card" onClick={onOpen}>
+          <Bot />
         </div>
+        <Modal backdrop="blur" isOpen={isOpen} onOpenChange={onOpenChange} size="5xl" scrollBehavior="inside">
+          <ModalContent>
+            {(onClose) => (
+              <>
+                <ModalHeader className="flex items-center justify-center gap-2">
+                  <FaRobot className="text-5xl" />
+                  JARS AI Chatbot
+                </ModalHeader>
+                <ModalBody>
+                  <Chatbot />
+                </ModalBody>
+              </>
+            )}
+          </ModalContent>
+        </Modal>
       </div>
     </>
   );
