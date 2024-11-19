87386ed427d68cad3ec7fa19c184430a2c01bf7d
rwick <trickrenzgarcia@gmail.com>
2024-11-19 18:07:14 +0800

    ok 200

--- src/lib/thirdweb-sdk.ts
+++ src/lib/thirdweb-sdk.ts
@@ -2,7 +2,7 @@ import { createThirdwebClient } from "thirdweb";
 import { polygon } from "thirdweb/chains";
 
 const clientId = process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID;
-const secretKey = process.env.THIRDWEB_API_KEY;
+const secretKey = process.env.NEXT_PUBLIC_THIRDWEB_API_KEY;
 const chain = polygon;
 
 const client = createThirdwebClient({
