c7c3e0ee1e91bd0f2729d375da52f188647615d6
rigs <rigorsyguatbsn@gmail.com>
2024-11-27 21:49:51 +0800

    change style

--- src/app/(routes)/collection/_components/AddressButton.tsx
+++ src/app/(routes)/collection/_components/AddressButton.tsx
@@ -1,25 +1,26 @@
-"use client"
+"use client";
 
-import { Button } from '@/components/ui/button'
-import { polygonScan, shortenWalletAddress } from '@/lib/utils';
-import { ExternalLinkIcon, LinkIcon } from 'lucide-react';
-import { useRouter } from 'next/navigation';
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { polygonScan, shortenWalletAddress } from "@/lib/utils";
+import { ExternalLinkIcon, LinkIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+import React from "react";
 
 type Props = {
   address: string;
-}
+};
 
 export default function AddressButton({ address }: Props) {
   return (
     <p
-      className='flex gap-1 items-center text-[11px] bg-[#F3F3F3] dark:bg-[#272727] text-[#272727] dark:text-[#F3F3F3] py-1 px-2 border-muted-foreground rounded-sm w-fit cursor-pointer'
+      className="mt-2 flex w-fit cursor-pointer items-center gap-2 rounded-sm border-muted-foreground bg-[#F3F3F3] p-2 text-sm text-[#272727] dark:bg-[#272727] dark:text-[#F3F3F3]"
       onClick={() => {
         const url = polygonScan(address);
         window.open(url, "_blank", "noopener,noreferrer");
       }}
     >
-      {shortenWalletAddress(address)} <ExternalLinkIcon className='h-4 w-4' />
+      <p>{shortenWalletAddress(address)}</p>
+      <ExternalLinkIcon className="h-4 w-4" />
     </p>
-  )
+  );
 }
