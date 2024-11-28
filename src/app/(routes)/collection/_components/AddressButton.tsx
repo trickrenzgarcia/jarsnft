df5ac0e281b3491446e76460bf1efdb81acbee0a
rigs <rigorsyguatbsn@gmail.com>
2024-11-28 18:02:24 +0800

    fix hydration error

--- src/app/(routes)/collection/_components/AddressButton.tsx
+++ src/app/(routes)/collection/_components/AddressButton.tsx
@@ -12,8 +12,8 @@ type Props = {
 
 export default function AddressButton({ address }: Props) {
   return (
-    <p
-      className="flex w-fit cursor-pointer items-center gap-2 rounded-sm border-muted-foreground bg-[#F3F3F3] p-2 text-sm text-[#272727] dark:bg-[#272727] dark:text-[#F3F3F3]"
+    <Button
+      className="flex w-fit items-center gap-2 rounded-sm border-muted-foreground bg-[#F3F3F3] p-2 text-sm text-[#272727] hover:bg-[#f3f3f3d5] dark:bg-[#272727] dark:text-[#F3F3F3] dark:hover:dark:bg-[#272727d7]"
       onClick={() => {
         const url = polygonScan(address);
         window.open(url, "_blank", "noopener,noreferrer");
@@ -21,6 +21,6 @@ export default function AddressButton({ address }: Props) {
     >
       <p>{shortenWalletAddress(address)}</p>
       <ExternalLinkIcon className="h-4 w-4" />
-    </p>
+    </Button>
   );
 }
